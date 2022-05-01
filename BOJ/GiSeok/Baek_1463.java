846f81079c2e28d77c08e3d9a159ada8c7d13936
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-05-01 22:10:06 +0900

    동적 계획법 01

---
+++ BOJ/GiSeok/Baek_1463.java
@@ -0,0 +1,35 @@
+package BOJ.GiSeok;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Baek_1463 {
+    public static int inspect(int n) {
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i < n+1; i++) {
+            if (i%3 == 0) {
+                dp[i] = Math.min(dp[i-1], dp[i/3]) + 1;
+                if (i%2 == 0)       // 최소 공배수 6을 고려하기 위한 조건문,,
+                    dp[i] = Math.min(dp[i], dp[i/2]+1); 
+            }
+            else if (i%2 == 0)
+                dp[i] = Math.min(dp[i-1], dp[i/2]) + 1; 
+            else
+                dp[i] = dp[i-1] + 1;
+        }
+
+        return dp[n];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int num = inspect(N);
+
+        System.out.println(num);
+    }
+}
