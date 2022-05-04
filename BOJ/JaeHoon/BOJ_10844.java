189fffbd730f25e64f2df7472d414cdb36c0b242
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-04 14:47:38 +0900

    DP: BOJ_10844 쉬운 계단 수(05/04)

---
+++ BOJ/JaeHoon/BOJ_10844.java
@@ -0,0 +1,32 @@
+package BOJ.JaeHoon;
+
+import java.util.Scanner;
+
+public class BOJ_10844 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[][] dp = new int[101][10];
+    for(int i=1; i<=9; i++) {
+      dp[1][i] = 1;
+    }
+    for(int i=2; i<=N; i++) {
+      for(int j=0; j<=9; j++) {
+        if(j == 0) {
+          dp[i][j] = dp[i-1][j+1] % 1_000_000_000;
+        }
+        else if(j == 9) {
+          dp[i][j] = dp[i-1][j-1] % 1_000_000_000;
+        }else {
+          dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1_000_000_000;
+        }
+      }
+    }
+    int sum = 0;
+    for(int i=0; i<10; i++) {
+      sum += dp[N][i];
+      sum %= 1_000_000_000;
+    }
+    System.out.println(sum);
+  }
+}
