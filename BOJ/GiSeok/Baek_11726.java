05af3153e8d21766661a2650900dcf4899dc126d
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-05-05 23:32:49 +0900

    BaekJoon - 11726 2xN 타일링  (05/04))

--- BOJ/GiSeok/Baek_11726.java
+++ BOJ/GiSeok/Baek_11726.java
@@ -1,12 +1,11 @@
-package BOJ.GiSeok;
-
 /*
 BaekJoon - 11726 2xN 타일링 (05/04 수)
 
 1. 더할 수록 사이즈가 기하급수적으로 커져서 분배법칙을 이용해 더하기 전에 나머지를 구한다.
    나머지 분배법칙 (A + B) % p = ((A % p) + (B % p)) % p
 2. 어차피 long형이라서 둘이 더해서 나머지를 구하면 됨
- */
+*/
+package BOJ.GiSeok;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
