2e9b82ab790a135fe1482e67e6d46a7e998d6e33
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-05-19 21:55:13 +0900

    백준 - 2748 피보나치 수 2 (05/19)

---
+++ BOJ/GiSeok/Baek_2748.java
@@ -0,0 +1,23 @@
+/*
+백준 - 2748 피보나치 수 2 (05/19 목)
+
+*/
+package BOJ.GiSeok;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Baek_2748 {
+    public static void main(String[] args) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 2];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < N + 1; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+
+        System.out.println(dp[N]);
+    }
+}
