e4f3050fe9a85789481e17d007d4628b822fa2a1
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-05-21 19:09:35 +0900

    백준 - 14501 퇴사 (05/21수정

--- BOJ/GiSeok/Baek_14501.java
+++ BOJ/GiSeok/Baek_14501.java
@@ -12,28 +12,29 @@ public class Baek_14501 {
     public static void main(String[] args) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[2][N + 2];
+        int[][] dp = new int[N + 2][2];
 
         for (int i = 1; i < N + 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            dp[0][i] = Integer.parseInt(st.nextToken());
-            dp[1][i] = Integer.parseInt(st.nextToken());
+            dp[i][0] = Integer.parseInt(st.nextToken());
+            dp[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = N; i > 0; i--) {
-            if (i + (dp[0][i]-1) > N)
-                dp[1][i] = 0;
+            int Ti = i + (dp[i][0]-1);
+            if (Ti > N)
+                dp[i][1] = 0;
             else {
                 int max = 0;
-                for (int j = (i + dp[0][i]); j < N + 1; j++)         
-                    max = Math.max(max, dp[1][j]);
-                dp[1][i] += max;
+                for (int j = (Ti + 1); j < N + 1; j++)         
+                    max = Math.max(max, dp[j][1]);
+                dp[i][1] += max;
             }
         }
 
         int max = dp[1][1];
         for (int i = 2; i < N + 1; i++) {
-            max = Math.max(max, dp[1][i]);
+            max = Math.max(max, dp[i][1]);
         }
 
         System.out.println(max);
