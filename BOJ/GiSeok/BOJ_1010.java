1d0949c6adc7753b0f5d7ab5ff8a64109ba22241
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-05-24 00:44:58 +0900

    백준 - 1010 다리 놓기 (05/23)

--- BOJ/GiSeok/BOJ_1010.java
+++ BOJ/GiSeok/BOJ_1010.java
@@ -5,43 +5,35 @@
 package BOJ.GiSeok;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class BOJ_1010 {
     public static void main(String[] args) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int tcase = Integer.parseInt(br.readLine());
         int[][] dp = new int[32][32];
-        int[][] NM = new int[tcase][2];
+
         for (int i = 1; i < 31 + 1; i++)
                 dp[1][i] = i;
+
+        for (int i = 2; i < 31 + 1; i++) {
+            for (int j = i; j < 31 + 1; j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j-1];
+            }
+        }
         
-        int maxN = 0;
-        int maxM = 0;
         for (int cnt = 0; cnt < tcase; cnt++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            NM[cnt][0] = N;
-            NM[cnt][1] = M;
-            
-            if (maxN < N)
-                maxN = N;
-            if (maxM < M)
-                maxM = M;
+            bw.write(dp[N][M] + "\n");
         }
-
-        for (int i = 2; i < maxN + 1; i++) {
-            for (int j = i; j < maxM + 1; j++) {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j-1];
-            }
-        }
-        
-        for (int i = 0; i < tcase; i++) {
-            System.out.println(dp[NM[i][0]][NM[i][1]]);
-        }
+        bw.flush();
+        bw.close();
     }
 }
