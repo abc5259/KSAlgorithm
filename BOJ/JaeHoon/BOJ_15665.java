8987bcec481e0ef3b3628ca94dcf295074496855
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-27 00:15:50 +0900

    백트래킹 - BOJ_15665 N과 M (11) (05/26)

---
+++ BOJ/JaeHoon/BOJ_15665.java
@@ -0,0 +1,42 @@
+package BOJ.JaeHoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_15665 {
+  public static int N;
+  public static int M;
+  public static int[] arr;
+  public static int[] nums;
+  public static StringBuffer sb = new StringBuffer();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    M = sc.nextInt();
+    arr = new int[M];
+    nums = new int[N+1];
+    for(int i=1; i<=N; i++) {
+      nums[i] = sc.nextInt();
+    }
+    Arrays.sort(nums);
+    func(0);
+    System.out.println(sb);
+  }
+  public static void func(int depth) {
+    if(depth == M) {
+      for(int i=0; i<M; i++) {
+        sb.append(arr[i] + " ");
+      }
+      sb.append("\n");
+      return;
+    }
+    int pick = 0;
+    for(int i=1; i<=N; i++) {
+      if(pick != nums[i]) {
+        arr[depth] = nums[i];
+        pick = nums[i];
+        func(depth + 1);
+      }
+    }
+  }
+}
