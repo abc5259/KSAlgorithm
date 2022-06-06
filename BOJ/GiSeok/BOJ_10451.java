974c318650df60033427448c5be6887ca5a0e6a3
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-06-06 12:14:59 +0900

    백준 - 10451 순열 그래프 (06/06)

---
+++ BOJ/GiSeok/BOJ_10451.java
@@ -0,0 +1,69 @@
+/*
+백준 - 10451 순열 그래프 (06/06 월)
+
+*/
+package BOJ.GiSeok;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+class Graph {
+    public ArrayList<ArrayList<Integer>> graph;
+    public boolean[] visited;
+    public int N;
+
+    Graph(int N) {
+        graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < N + 1; i++)
+            graph.add(new ArrayList<Integer>());
+        visited = new boolean[N + 1];
+    }
+    public void addEdge(int n1, int n2) {
+            graph.get(n1).add(n2);
+    }
+
+    public void dfs(int v) {
+        visited[v] = true;
+
+        for (int i = 0; i < graph.get(v).size(); i++) {
+            int p = graph.get(v).get(i);
+            if (!visited[p])
+                dfs(p);
+        }
+    }
+}
+
+public class BOJ_10451 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            int N = Integer.parseInt(br.readLine());
+            Graph gr = new Graph(N);
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int m = 1; m < N + 1; m++)
+                gr.addEdge(m, Integer.parseInt(st.nextToken()));
+            
+            int num = 0;
+            for (int j = 1; j < N + 1; j++) {
+                if (!gr.visited[j]) {
+                    gr.dfs(j);
+                    num++;
+                }
+            }
+
+            bw.write(num + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
