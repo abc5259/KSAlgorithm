08c763cff4589c5dbce220b1ca58045a75c78427
LeeJaeHoon <dlwogns3413@naver.com>
2022-07-05 15:24:42 +0900

    BOJ_16947[그래프] - 서울 지하철 2호선

--- BOJ/JaeHoon/BOJ_16929.java
+++ BOJ/JaeHoon/BOJ_16929.java
@@ -3,8 +3,6 @@ package BOJ.JaeHoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_16929 {
@@ -30,14 +28,20 @@ public class BOJ_16929 {
     }
     for(int i=0; i<N; i++) {
       for(int j=0; j<M; j++) {
-        isVisit = new boolean[N][M];
-        Point point = new Point(i, j, map[i][j], 0);
-        dfs(point, point);
+        if(!isVisit[i][j]) {
+          // isVisit = new boolean[N][M];
+          Point point = new Point(i, j, map[i][j], 0);
+          boolean cand = dfs(point, point);
+          if(cand) {
+            System.out.println("Yes");
+            System.exit(0);
+          }
+        }
       }
     }
-    System.out.println(ok ? "Yes" : "No");
+    System.out.println("No");
   }
-  public static void dfs(Point point,Point startPoint)  {
+  public static boolean dfs(Point point,Point startPoint)  {
     isVisit[point.x][point.y] =  true;
     for(int i=0; i<4; i++) {
       int x = point.x + dir[i][0];
@@ -45,19 +49,17 @@ public class BOJ_16929 {
       if(x >=0 && x < N && y >= 0 && y < M) {
         if(!isVisit[x][y] && map[x][y] == point.color) {
           Point p = new Point(x, y, point.color, point.count + 1);
-          dfs(p,startPoint);
+          if(dfs(p,startPoint)) return true;
         }else if(isVisit[x][y] 
                 && x == startPoint.x 
-                && point.color == startPoint.color
                 && y == startPoint.y
                 && point.count >= 3) 
         {
-          System.out.println("x = " + x + " y = " + y + " cnt = " + point.count);
-          ok = true;
-          return;
+          return true;
         }
       }
     }
+    return false;
   }
 }
 class Point {
---
+++ BOJ/JaeHoon/BOJ_16947.java
@@ -0,0 +1,91 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_16947 {
+  static ArrayList<ArrayList<Integer>> graph;
+  static boolean[] isVisit;
+  static boolean[] cycle;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    StringBuffer sb = new StringBuffer();
+    graph = new ArrayList<>();
+    isVisit = new boolean[N+1];
+    cycle = new boolean[N+1];
+    for(int i=0; i<=N; i++) {
+      graph.add(new ArrayList<>());
+    }
+    for(int i=0; i<N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int v1 = Integer.parseInt(st.nextToken());
+      int v2 = Integer.parseInt(st.nextToken());
+      graph.get(v1).add(v2);
+      graph.get(v2).add(v1);
+    }
+    for(int i=1; i<=N; i++) {
+      isCycle(i,i,i);
+      isVisit = new boolean[N+1];
+    }
+    for(int i=1; i<=N; i++) {
+      if(!cycle[i]) {
+        sb.append(bfs(i)).append(' ');
+        isVisit = new boolean[N+1];
+      }else {
+        sb.append(0).append(' ');
+      }
+    }
+    System.out.println(sb);
+  }
+  public static boolean isCycle(int prev,int node,int start) {
+    isVisit[node] = true;
+    for(int V: graph.get(node)) {
+      if(!isVisit[V]) {
+        if(isCycle(node,V,start)) return true;
+      }
+      else if(V != prev && V == start) {
+        cycle[start] = true;
+        return true;
+      }
+    }
+    return false;
+  }
+  public static int bfs(int start) {
+    Queue<Node> queue = new LinkedList<>();
+    Node startNode = new Node(start,0);
+    queue.offer(startNode);
+    isVisit[start] = true;
+    int count = 0;
+    while(!queue.isEmpty()) {
+      Node node = queue.poll();
+      for(int V: graph.get(node.node)) {
+        Node next = new Node(V, node.count+1);
+        if(cycle[V]) {
+          count = next.count;
+          break;
+        }
+        else if(!isVisit[V] && !cycle[V]) {
+          queue.offer(next);
+          isVisit[V] = true;
+        }
+      }
+    }
+    return count;
+  }
+  public static class Node {
+    int count;
+    int node;
+    Node(int node,int count) {
+      this.count = count;
+      this.node = node;
+    }
+  }
+}
