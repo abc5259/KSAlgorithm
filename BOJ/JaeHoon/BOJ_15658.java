49c31fbf9a3a83e6fd93b5e08db4c2e064f6a1e9
LeeJaeHoon <dlwogns3413@naver.com>
2022-07-14 13:35:19 +0900

    BOJ_15658[백트래킹] - 연산자 끼워넣기(2) (07/14)

--- BOJ/JaeHoon/BOJ_15658.java
+++ BOJ/JaeHoon/BOJ_15658.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class BOJ_15658 {
   static int[] arr;
-  static int[] operator = new int[4]; 
+  static int[] operator; 
   static int N;
   static int min = Integer.MAX_VALUE;
   static int max = Integer.MIN_VALUE;
@@ -16,6 +16,7 @@ public class BOJ_15658 {
     N = Integer.parseInt(br.readLine());
     StringTokenizer st = new StringTokenizer(br.readLine());
     arr = new int[N];
+    operator = new int[4];
     for(int i=0; i<N; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
