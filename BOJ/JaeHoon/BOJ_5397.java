24239a66651dd2f3d66c30426b669cb7b4c2b829
LeeJaeHoon <dlwogns3413@naver.com>
2022-10-30 17:45:17 +0900

    스택 - 키로거

---
+++ BOJ/JaeHoon/BOJ_5397.java
@@ -0,0 +1,54 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BOJ_5397 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int L = Integer.parseInt(br.readLine());
+    String[] arr = new String[L];
+    StringBuffer sb = new StringBuffer();    
+    for(int i=0; i<L; i++) {
+      arr[i] = br.readLine();
+      sb.append(solove(arr[i])).append('\n');
+    }
+    System.out.println(sb);
+  }
+  static public String solove(String str) {
+    Stack<Character> stack = new Stack<>();
+    Stack<Character> stack2 = new Stack<>();
+    for(int i=0; i<str.length(); i++) {
+      char c = str.charAt(i);
+      switch(c) {
+        case '<':
+          if(!stack.isEmpty()) 
+            stack2.push(stack.pop());
+          break;
+        case '>':
+          if(!stack2.isEmpty())
+            stack.push(stack2.pop());
+          break;
+        case '-':
+          if(!stack.isEmpty())
+            stack.pop();
+          break;
+        default: 
+          stack.push(c);
+          break;
+      }
+    }
+    while(!stack2.isEmpty()) {
+      stack.push(stack2.pop());
+    }
+    StringBuffer st = new StringBuffer();
+    while(!stack.isEmpty()) {
+      st.append(stack.pop());
+    }
+    return st.reverse().toString();
+  }
+}
