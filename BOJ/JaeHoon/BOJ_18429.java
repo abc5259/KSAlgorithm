51c8def0a204b2e958900f07b0e2995eb0197206
LeeJaeHoon <dlwogns3413@naver.com>
2022-11-18 19:10:00 +0900

    브루트포스 - 근손실

---
+++ BOJ/JaeHoon/BOJ_18429.java
@@ -0,0 +1,55 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_18429 {
+  static int N,K;
+  static int[] kits;
+  static int[] arr;
+  static boolean[] isVisit;
+  static int answer;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+    kits = new int[N];
+    arr = new int[N];
+    isVisit = new boolean[N];
+
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; i++) {
+      kits[i] = Integer.parseInt(st.nextToken());
+    }
+    solve(0);
+    System.out.println(answer);
+  }
+  public static void solve(int depth) {
+    if(depth == N) {
+      if(check()) answer++;
+      return;
+    }
+
+    for(int i=0; i<N; i++) {
+      if(!isVisit[i]) {
+        arr[depth] = kits[i];
+        isVisit[i] = true;
+        solve(depth+1);
+        isVisit[i] = false;
+        arr[depth] = 0;
+      }
+    }
+  }
+  public static boolean check() {
+    int weight = 500;
+    for(int i=0; i<N; i++) {
+      weight += arr[i] - K;
+      if(weight < 500) return false;
+    }
+    return true;
+  }
+}
