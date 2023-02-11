15ab3e921bcca7c030fa750a63b45004ca34db70
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2023-02-11 21:13:06 +0900

    백트래킹 - N과 M (4)

---
+++ BOJ/GiSeok/Java/BOJ_15652.java
@@ -0,0 +1,41 @@
+package BOJ.GiSeok.Java;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15652 {
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static boolean[] visited;
+    public static StringBuilder sb;
+
+    public static void backtrack(int num, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < cnt; i++)
+                sb.append(arr[i] + " ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = num; i < N+1; i++) {
+            arr[cnt] = i;
+            backtrack(i, cnt+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        visited = new boolean[N+1];
+        arr = new int[M];
+
+        backtrack(1, 0);
+        System.out.println(sb);
+    }
+}
