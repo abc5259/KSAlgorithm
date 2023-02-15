3fc77aced18ade62f4fd70ae3f5cb821e1d63ca1
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2023-02-15 19:35:19 +0900

    그리디 - 센서

---
+++ BOJ/GiSeok/Java/BOJ_2212.java
@@ -0,0 +1,34 @@
+package BOJ.GiSeok.Java;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_2212 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        int[] streets = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            streets[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(streets);
+
+        int ans = 0;
+        if (K < N) {
+            int[] dists = new int[N];
+            for (int i = 0; i < N-1; i++)
+                dists[i] = streets[i+1] - streets[i];
+
+            Arrays.sort(dists);
+            for (int i = (N-1)-(K-1); i >= 0; i--)
+                ans += dists[i];
+        }
+
+        System.out.println(ans);
+    }
+}
