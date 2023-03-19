a9bdd218f37f8e72c5389b7344bf83516af70424
LeeJaeHoon <dlwogns3413@naver.com>
2023-03-19 23:00:48 +0900

    백트래킹 - 스도쿠(비트마스킹 풀이) [retry]

--- BOJ/JaeHoon/retry/BOJ_2580.java
+++ BOJ/JaeHoon/retry/BOJ_2580.java
@@ -9,6 +9,9 @@ import java.util.StringTokenizer;
 public class BOJ_2580 {
   static int[][] map;
   static ArrayList<int[]> zeroList = new ArrayList<>();
+  static int[] isRowUsed = new int[9];
+  static int[] isColUsed = new int[9];
+  static int[][] isSquareUsed = new int[3][3];
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = null;
@@ -20,6 +23,11 @@ public class BOJ_2580 {
       for(int j=0; j<9; j++) {
         map[i][j] = Integer.parseInt(st.nextToken());
         if(map[i][j] == 0) zeroList.add(new int[]{i,j});
+        else {
+          isRowUsed[i] |= 1 << map[i][j];
+          isColUsed[j] |= 1 << map[i][j];
+          isSquareUsed[i/3][j/3] |= 1 << map[i][j];
+        }
       }
     }
     StringBuffer sb = new StringBuffer();
@@ -33,6 +41,7 @@ public class BOJ_2580 {
       }
       sb.append("\n");
     }
+
     System.out.println(sb);
   }
   public static boolean solve(int depth) {
@@ -43,25 +52,19 @@ public class BOJ_2580 {
     int row = zeroList.get(depth)[0];
     int col = zeroList.get(depth)[1];
 
-    boolean[] isUsed = new boolean[10];
-    for(int i=0; i<9; i++) {
-      if(map[row][i] != 0) isUsed[map[row][i]] = true;
-      if(map[i][col] != 0) isUsed[map[i][col]] = true;
-    }
-
-    int x = row / 3 * 3;
-    int y = col / 3 * 3;
-
-    for(int i=x; i<x+3; i++) {
-      for(int j=y; j<y+3; j++) {
-        if(map[i][j] != 0) isUsed[map[i][j]] = true;
-      }
-    }
-
     for(int i=1; i<10; i++) {
-      if(!isUsed[i]) {
+      if(((isRowUsed[row] >> i) & 1) == 0 && 
+         ((isColUsed[col] >> i) & 1) == 0 && 
+         ((isSquareUsed[row/3][col/3] >> i) & 1) == 0) {
+          
+        isRowUsed[row] |= 1 << i;
+        isColUsed[col] |= 1 << i;
+        isSquareUsed[row/3][col/3] |= 1 << i;
         map[row][col] = i;
         if(solve(depth+1)) return true;
+        isRowUsed[row] &= ~(1 << i);
+        isColUsed[col] &= ~(1 << i);
+        isSquareUsed[row/3][col/3] &= ~(1 << i);
         map[row][col] = 0;
       }
     }
