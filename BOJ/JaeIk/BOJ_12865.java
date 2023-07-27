acc2c71e3174777b777bad72354fb1441a79b99d
Choijake <shameless8@naver.com>
2023-07-27 22:39:18 +0900

    dp-평범한 배낭

--- BOJ/JaeIk/BOJ_12865.java
+++ BOJ/JaeIk/BOJ_12865.java
@@ -1,47 +1,44 @@
 package BOJ.JaeIk;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class BOJ_12865 {
+    static int[] W;
+    static int[] V;
     static Integer[][] dp;
-    static int[] W; // weight
-    static int[] V; // value
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        W = new int[N];
-        V = new int[N];
+        W = new int[n];
+        V = new int[n];
 
         //배열의 0이 아닌 1번째 부터 채운다
-        dp = new Integer[N][K + 1];
+        dp = new Integer[n][k+1];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
             W[i] = Integer.parseInt(st.nextToken());
             V[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(knapsack(N - 1, K));
+        System.out.println(knapsack(n-1,k));
 
     }
 
-    static int knapsack(int i, int k) {
-        // i가 0미만, 즉 범위 밖이 된다면
-        if (i < 0)
+    static int knapsack(int i, int k){
+        if(i<0){
             return 0;
+        }
 
-        // 탐색하지 않은 위치라면?
-        if (dp[i][k] == null) {
+
+        if(dp[i][k]==null){
 
             //현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색)
             //예를 들어 i=3 dp=3일 때는 3번째의 가치 6을 가진다
