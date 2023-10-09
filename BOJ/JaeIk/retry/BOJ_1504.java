0103abac8a277a453dc00cf4803b525d2578f55b
Choijake <shameless8@naver.com>
2023-10-09 22:58:48 +0900

    최단경로-특정한최단경로

--- BOJ/JaeIk/retry/BOJ_1504.java
+++ BOJ/JaeIk/retry/BOJ_1504.java
@@ -1,109 +1,103 @@
 package BOJ.JaeIk.retry;
 
-
 import java.io.*;
 import java.util.*;
 
-class Point implements Comparable<Point>{
+class Vertex implements Comparable<Vertex>{
     int end;
     int weight;
 
-    public Point(int end, int weight){
+    public Vertex(int end, int weight){
         this.end = end;
         this.weight = weight;
     }
 
     @Override
-    public int compareTo(Point p) {
-        return weight - p.weight;
+    public int compareTo(Vertex o) {
+        return weight - o.weight;
     }
 }
+
 public class BOJ_1504 {
-    static boolean[] visited;
-    static int INF = 200_000_000;
+    static int INF = 200000000;
+    static ArrayList<ArrayList<Vertex>> list;
     static int[] dist;
-    static List<Point>[] list;
-    static int N, E;
-    static int a,b,c;
+    static int N,E;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N+1];
+
+        list = new ArrayList<>();
+
         dist = new int[N+1];
-        visited = new boolean[N+1];
 
-        for(int i=1; i<=N; i++){
-            list[i] = new ArrayList<>();
+        for(int i=0; i<=N; i++){
+            list.add(new ArrayList<>());
         }
+        Arrays.fill(dist, INF);
 
+        //양방향 인접 리스트
         for(int i=0; i<E; i++){
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
 
-            list[a].add(new Point(b, c));
-            //무방향그래프
-            list[b].add(new Point(a, c));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            list.get(start).add(new Vertex(end, weight));
+
+            list.get(end).add(new Vertex(start, weight));
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         st = new StringTokenizer(br.readLine());
-        int require1 = Integer.parseInt(st.nextToken());
-        int require2 = Integer.parseInt(st.nextToken());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
 
-        int answer = solve(require1, require2);
-        bw.write(answer+ "\n");
+        int result1=0;
+        result1 += dijkstra(1,v1);
+        result1 += dijkstra(v1, v2);
+        result1 += dijkstra(v2, N);
 
+        int result2=0;
+        result2 += dijkstra(1,v2);
+        result2 += dijkstra(v2, v1);
+        result2 += dijkstra(v1, N);
+
+        int result = (result1>=INF && result2>=INF) ? -1 : Math.min(result1, result2);
+
+        bw.write(result + "\n");
+        bw.flush();
         bw.close();
         br.close();
-    }
 
-    static int solve(int req1, int req2){
-        int result1 = 0;
-        int result2 = 0;
-
-        result1 += dijkstra(1, req1);
-        result1 += dijkstra(req1, req2);
-        result1 += dijkstra(req2, N);
-
-        result2 += dijkstra(1, req2);
-        result2 += dijkstra(req2, req1);
-        result2 += dijkstra(req1, N);
-
-        // 경로1 && 경로2 -> 가는길이 없는 경우
-        if(result1 >= INF && result2 >= INF) return -1;
-        else return Math.min(result1, result2);
     }
 
     static int dijkstra(int start, int end){
+        PriorityQueue<Vertex> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[N+1];
         Arrays.fill(dist, INF);
+
         Arrays.fill(visited, false);
 
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-
-        pq.add(new Point(start, 0));
+        pq.offer(new Vertex(start, 0));
         dist[start] = 0;
 
         while(!pq.isEmpty()){
-            Point curPoint = pq.poll();
+            Vertex cur = pq.poll();
+            int next = cur.end;
 
-            int cur = curPoint.end;
-            int curWeight = curPoint.weight;
+            if(!visited[next]){
+                visited[next] = true;
 
-            if(visited[cur]==true)continue;
-            visited[cur] = true;
-
-            for(int i=0; i<list[cur].size(); i++){
-                int nextPoint = list[cur].get(i).end;
-                int nextWeight = list[cur].get(i).weight;
-
-                if(!visited[nextPoint] && dist[nextPoint]>curWeight+nextWeight){
-                    dist[nextPoint] = curWeight + nextWeight;
-                    pq.add(new Point(nextPoint, dist[nextPoint]));
+                for(Vertex v : list.get(next)){
+                    if(!visited[v.end] && dist[v.end]>dist[next]+v.weight){
+                        dist[v.end] = dist[next]+v.weight;
+                        pq.add(new Vertex(v.end, dist[v.end]));
+                    }
                 }
             }
         }
--- BOJ/JaeIk/retry/BOJ_1753.java
+++ BOJ/JaeIk/retry/BOJ_1753.java
@@ -1,90 +1,91 @@
-package BOJ.JaeIk.retry;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-class Node implements Comparable<Node>{
-    int end, weigth;
-
-    public Node(int end, int weight){
-        this.end = end;
-        this.weigth = weight;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return weigth - o.weigth;
-    }
-}
-
-public class BOJ_1753 {
-    static final int INF = 100_000_000;
-    static List<Node>[] list;
-    static int[] dist;
-    static int V, E, S;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(br.readLine());
-
-        list = new ArrayList[V+1];
-
-        dist = new int[V+1];
-
-        Arrays.fill(dist, INF);
-
-        for(int i=1; i<V+1; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        for(int i=0; i<E; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            list[start].add(new Node(end, weight));
-        }
-
-        dijkstra(S);
-
-        for(int i=1; i<=V; i++){
-            if(dist[i] == INF)sb.append("INF\n");
-            else sb.append(dist[i] + "\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    static void dijkstra(int start){
-        PriorityQueue<Node> q = new PriorityQueue<>();
-
-        boolean[] visited = new boolean[V+1];
-
-        q.add(new Node(start, 0));
-
-        dist[start] = 0;
-
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-            int next = cur.end;
-
-            if(visited[next])continue;
-
-            visited[next] = true;
-
-            for(Node node : list[next]){
-                if(dist[node.end] > dist[next] + node.weigth){
-                    dist[node.end] = dist[next] + node.weigth;
-                    q.add(new Node(node.end, dist[node.end]));
-                }
-            }
-        }
-    }
-}
+//package BOJ.JaeIk.retry;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.*;
+//
+//class Node implements Comparable<Node>{
+//    int end, weigth;
+//
+//    public Node(int end, int weight){
+//        this.end = end;
+//        this.weigth = weight;
+//    }
+//
+//    //우선순위 큐에서 가중치가 작은 것부터 poll
+//    @Override
+//    public int compareTo(Node o) {
+//        return weigth - o.weigth;
+//    }
+//}
+//
+//public class BOJ_1753 {
+//    static final int INF = 100_000_000;
+//    static List<Node>[] list;
+//    static int[] dist;
+//    static int V, E, S;
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        StringBuilder sb = new StringBuilder();
+//
+//        V = Integer.parseInt(st.nextToken());
+//        E = Integer.parseInt(st.nextToken());
+//        S = Integer.parseInt(br.readLine());
+//
+//        list = new ArrayList[V+1];
+//
+//        dist = new int[V+1];
+//
+//        Arrays.fill(dist, INF);
+//
+//        for(int i=1; i<V+1; i++){
+//            list[i] = new ArrayList<>();
+//        }
+//
+//        for(int i=0; i<E; i++){
+//            st = new StringTokenizer(br.readLine());
+//            int start = Integer.parseInt(st.nextToken());
+//            int end = Integer.parseInt(st.nextToken());
+//            int weight = Integer.parseInt(st.nextToken());
+//
+//            list[start].add(new Node(end, weight));
+//        }
+//
+//        dijkstra(S);
+//
+//        for(int i=1; i<=V; i++){
+//            if(dist[i] == INF)sb.append("INF\n");
+//            else sb.append(dist[i] + "\n");
+//        }
+//
+//        System.out.println(sb);
+//    }
+//
+//    static void dijkstra(int start){
+//        PriorityQueue<Node> q = new PriorityQueue<>();
+//
+//        boolean[] visited = new boolean[V+1];
+//
+//        q.add(new Node(start, 0));
+//
+//        dist[start] = 0;
+//
+//        while(!q.isEmpty()){
+//            Node cur = q.poll();
+//            int next = cur.end;
+//
+//            if(visited[next])continue;
+//
+//            visited[next] = true;
+//
+//            for(Node node : list[next]){
+//                if(dist[node.end] > dist[next] + node.weigth){
+//                    dist[node.end] = dist[next] + node.weigth;
+//                    q.add(new Node(node.end, dist[node.end]));
+//                }
+//            }
+//        }
+//    }
+//}
