69341b844bcc729763a1f6a7f5cd0e04c1926c0e
LeeJaeHoon <dlwogns3413@naver.com>
2023-12-07 16:16:54 +0900

    MST - 복제 로봇

--- BOJ/JaeHoon/BOJ_10711.java
+++ BOJ/JaeHoon/BOJ_10711.java
@@ -3,10 +3,8 @@ package BOJ.JaeHoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class BOJ_10711 {
@@ -30,10 +28,6 @@ public class BOJ_10711 {
             String s = br.readLine();
             for(int j=0; j<M; j++) {
                 map[i][j] = s.charAt(j);
-//                if(map[i][j] != '.' && map[i][j] != '9') {
-////                    System.out.println(" i = " + i + " j = " + j);
-//
-//                }
             }
         }
 
