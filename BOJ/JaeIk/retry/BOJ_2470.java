eb02d7307dcea1231a963164ee7d446a1b237ba6
Choijake <shameless8@naver.com>
2024-01-20 18:41:32 +0900

    투포인터-두 용액

--- BOJ/JaeIk/retry/BOJ_2470.java
+++ BOJ/JaeIk/retry/BOJ_2470.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class BOJ_2470 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
