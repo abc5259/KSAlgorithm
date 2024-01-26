05047276cbef29aea386144fa2b61fc7057df208
Choijake <shameless8@naver.com>
2024-01-26 23:12:29 +0900

    백트래킹-n과1

--- BOJ/JaeIk/retry/BOJ_15649.java
+++ BOJ/JaeIk/retry/BOJ_15649.java
@@ -1,36 +1,35 @@
 package BOJ.JaeIk.retry;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_15649 {
-    static StringBuilder sb = new StringBuilder();
-    static Boolean[] visited;
-    static int arr[];
+    static boolean[] visited;
+    static StringBuilder sb;
+    static int[] arr;
     static int n, m;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[m];
+        visited = new boolean[n];
 
-        visited = new Boolean[n];
-        for(int i=0; i<visited.length; i++){
-            visited[i] = false;
-        }
-
-        solve(n, m, 0);
+        solve(0);
 
         System.out.println(sb);
     }
 
-    static void solve(int n, int m, int depth){
-        if(depth == m){
-            for(int val : arr){
-                sb.append(val).append(" ");
+    static void solve(int depth){
+        if(depth==m){
+            for(int number : arr){
+                sb.append(number).append(" ");
             }
             sb.append("\n");
             return;
@@ -39,9 +38,8 @@ public class BOJ_15649 {
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 visited[i] = true;
-                //크기가 m인 arr를 미리 초기화하지 않고 동적으로 입력해주어야한다
                 arr[depth] = i+1;
-                solve(n, m, depth+1);
+                solve(depth+1);
                 visited[i]=false;
             }
         }
