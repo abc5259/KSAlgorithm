0d9c1b014e0540d952cfab9304a0de7b6c1d7ae5
Choijake <shameless8@naver.com>
2024-01-30 01:29:21 +0900

    이분탐색-랜선자르기

---
+++ BOJ/JaeIk/practice/BOJ_1654.java
@@ -0,0 +1,49 @@
+package BOJ.JaeIk.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1654 {
+    static long low, mid, high, sum, max;
+    static long[] arr;
+    static int k, n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        max = Integer.MIN_VALUE;
+        arr = new long[k];
+        for(int i=0; i<k; i++){
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        low = 1;
+        high = max+1;
+
+        binarySearch();
+
+        System.out.println(low);
+    }
+    static boolean check(long mid){
+        for(long length : arr){
+            sum += length/mid;
+        }
+        return sum >= n;
+    }
+
+    static void binarySearch(){
+        while(low+1 < high){
+            sum = 0;
+            mid = (low+high)/2;
+
+            if(check(mid))low = mid;
+            else high = mid;
+        }
+    }
+}
