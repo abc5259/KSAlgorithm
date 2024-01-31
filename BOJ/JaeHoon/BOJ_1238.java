f82f7f18cda4918d1fc73c4ae29f51418d86975a
LeeJaeHoon <dlwogns3413@naver.com>
2024-01-31 19:55:48 +0900

    다익스트라 - 파티

--- BOJ/JaeHoon/BOJ_1238.java
+++ BOJ/JaeHoon/BOJ_1238.java
@@ -3,49 +3,77 @@ package BOJ.JaeHoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_1238 {
+    static class Node {
+        int v,t;
+
+        public Node(int v, int t) {
+            this.v = v;
+            this.t = t;
+        }
+    }
+    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    static ArrayList<ArrayList<Node>> rGraph = new ArrayList<>();
+    static int N,M,X;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-
-        int[][] dp = new int[N+1][N+1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
         for(int i=0; i<=N; i++) {
-            Arrays.fill(dp[i], 10000000);
-            dp[i][i] = 0;
+            graph.add(new ArrayList<>());
+            rGraph.add(new ArrayList<>());
         }
 
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
-            dp[s][e] = t;
+            graph.get(v1).add(new Node(v2, t));
+            rGraph.get(v2).add(new Node(v1, t));
         }
 
+
+        int[] cost1 = dijkstra(graph);
+        int[] cost2 = dijkstra(rGraph);
+
+        int answer = 0;
         for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
-                if (dp[j][i] == 10000000)
-                    continue;
-                for(int k=1; k<=N; k++) {
-                    dp[j][k] = Math.min(dp[j][k], dp[j][i] + dp[i][k]);
+            answer = Math.max(answer, cost1[i] + cost2[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+    static int[] dijkstra(ArrayList<ArrayList<Node>> graph) {
+        int[] cost = new int[N+1];
+        Arrays.fill(cost, 10000000);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        pq.offer(new int[]{X,0});
+        cost[X] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+
+            if(cost[curr[0]] < curr[1]) continue;
+
+            for(Node next: graph.get(curr[0])) {
+                if(cost[next.v] > next.t + curr[1]) {
+                    cost[next.v] = next.t + curr[1];
+                    pq.offer(new int[]{next.v, cost[next.v]});
                 }
             }
         }
 
-        int max = 0;
-
-        for(int i=1; i<=N; i++) {
-            max = Math.max(dp[i][X] + dp[X][i], max);
-        }
-
-        System.out.println(max);
+        return cost;
     }
 }
