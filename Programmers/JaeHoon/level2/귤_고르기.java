432b06780d8ef2566d1cefc471a88abbbc66da31
LeeJaeHoon <dlwogns3413@naver.com>
2024-03-04 12:34:02 +0900

    그리디 - 귤 고르기

---
+++ Programmers/JaeHoon/level2/귤_고르기.java
@@ -0,0 +1,29 @@
+package Programmers.JaeHoon.level2;
+
+import java.util.Arrays;
+
+public class 귤_고르기 {
+    import java.util.*;
+
+    class Solution {
+        public int solution(int k, int[] tangerine) {
+            int answer = 0;
+            int[] map = new int[10000001];
+
+            for(int i=0; i<tangerine.length; i++) {
+                map[tangerine[i]]++;
+            }
+
+            Arrays.sort(map);
+
+            for(int i=10000000; i>=0; i--) {
+                if(k <= 0) break;
+                if(map[i] == 0) break;
+
+                k -= map[i];
+                answer++;
+            }
+            return answer;
+        }
+    }
+}
