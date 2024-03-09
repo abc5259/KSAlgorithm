0236687d478554981aca1519bcca39fa66832de2
LeeJaeHoon <dlwogns3413@naver.com>
2024-03-10 02:18:16 +0900

    투 포인터 - 할인 행사

---
+++ Programmers/JaeHoon/level2/할인_행사.java
@@ -0,0 +1,38 @@
+package Programmers.JaeHoon.level2;
+
+import java.util.*;
+public class 할인_행사 {
+
+    class Solution {
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            Map<String, Integer> map = new HashMap<>();
+
+            for(int i=0; i<9; i++) {
+                map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
+            }
+
+            int left = 0;
+            int right = 9;
+
+            while(right < discount.length) {
+                map.put(discount[right], map.getOrDefault(discount[right], 0) + 1);
+
+                boolean isOk = true;
+                for(int i=0; i<want.length; i++) {
+                    if(map.getOrDefault(want[i], 0) < number[i]) {
+                        isOk = false;
+                        break;
+                    }
+                }
+
+                if(isOk) answer++;
+
+                map.put(discount[left], map.get(discount[left]) - 1);
+                left++;
+                right++;
+            }
+            return answer;
+        }
+    }
+}
