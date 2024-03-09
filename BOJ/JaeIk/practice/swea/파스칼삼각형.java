41571b8bcde4969445b3a1a470ef90eb6b33ece0
Choijake <shameless8@naver.com>
2024-03-09 23:57:51 +0900

    구현-파스칼삼각형

---
+++ BOJ/JaeIk/practice/swea/파스칼삼각형.java
@@ -0,0 +1,67 @@
+package BOJ.JaeIk.practice.swea;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class 파스칼삼각형
+{
+    public static void main(String args[]) throws Exception
+    {
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int t=0; t<T; t++){
+            int size = sc.nextInt();
+
+            int[][] map = pascal(size);
+
+            System.out.println("#"+(t+1));
+            for(int i=0; i<size; i++){
+                for(int j=0; j<size; j++){
+                    if(map[i][j]==0)continue;
+                    System.out.print(map[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    static int[][] pascal(int size){
+        int[][] map = new int[size][size];
+        for(int i=0; i<size; i++){
+            for(int j=0; j<=i; j++){
+                map[i][j] = 1;
+            }
+        }
+
+        for(int i=2; i<size; i++){
+            for(int j=1; j<i; j++){
+
+                map[i][j] = map[i-1][j-1] + map[i-1][j];
+
+            }
+        }
+
+        return map;
+    }
+}
