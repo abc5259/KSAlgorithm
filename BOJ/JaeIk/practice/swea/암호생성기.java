fbcb8ca4d9da65e3f7a6dc0ae874ea5bdeee67ab
Choijake <shameless8@naver.com>
2024-04-02 22:57:38 +0900

    암호생성기

---
+++ BOJ/JaeIk/practice/swea/암호생성기.java
@@ -0,0 +1,52 @@
+package BOJ.JaeIk.practice.swea;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class 암호생성기
+{
+    static Queue<Integer> crypt;
+
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int tc=0; tc<10; tc++) {
+            br.readLine();
+
+            crypt = new LinkedList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0; i<8; i++) {
+                crypt.add(Integer.parseInt(st.nextToken()));
+            }
+
+            boolean flag = true;
+            int now = crypt.peek();
+            while(flag) {
+                for(int i=1; i<=5; i++) {
+                    now = crypt.poll();
+                    int move = now-i;
+
+                    if(move<=0) {
+                        flag=false;
+                        crypt.add(0);
+                        break;
+                    }
+
+                    crypt.add(move);
+                }
+            }
+
+            System.out.print("#"+(tc+1));
+            for(int c : crypt) {
+                System.out.print(" "+c);
+            }
+            System.out.println();
+        }
+    }
+}
