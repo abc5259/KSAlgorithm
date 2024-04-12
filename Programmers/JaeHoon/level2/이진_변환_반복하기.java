82f374028afcc4a161422cfb104bfc4ede360d47
LeeJaeHoon <dlwogns3413@naver.com>
2024-04-12 20:56:17 +0900

    재귀 - 이진 변환 반복하기

---
+++ Programmers/JaeHoon/level2/이진_변환_반복하기.java
@@ -0,0 +1,49 @@
+package Programmers.JaeHoon.level2;
+
+public class 이진_변환_반복하기 {
+    class Solution {
+        static class Result {
+            int zeroCnt;
+            String str;
+            Result(int zeroCnt, String str) {
+                this.zeroCnt = zeroCnt;
+                this.str = str;
+            }
+        }
+        int cnt = 0;
+        public int[] solution(String s) {
+            int[] answer = new int[2];
+            answer[1] =  solve(s);
+            answer[0] = cnt;
+
+            return answer;
+        }
+
+        public int solve(String s) {
+            cnt++;
+
+            Result result = findZeroAndRemove(s);
+            int total = result.zeroCnt;
+            int length = result.str.length();
+            String binary = Integer.toBinaryString(length);
+            if(binary.equals("1")) return total;
+
+            return total + solve(binary);
+        }
+
+        public Result findZeroAndRemove(String s) {
+            StringBuilder sb  = new StringBuilder();
+            int zeroCnt = 0;
+
+            for(int i=0; i<s.length(); i++) {
+                if(s.charAt(i) == '0') {
+                    zeroCnt++;
+                }else {
+                    sb.append(s.charAt(i));
+                }
+            }
+
+            return new Result(zeroCnt, sb.toString());
+        }
+    }
+}
