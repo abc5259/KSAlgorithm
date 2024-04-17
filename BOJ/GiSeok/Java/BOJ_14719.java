3a1e623c5a59d402ead89f8955c53cd577223606
GiSeok Kang <ime.rokz@gmail.com>
2024-04-17 18:23:22 +0900

    14719 - 빗물(투포인터)

--- BOJ/GiSeok/Java/BOJ_14719.java
+++ BOJ/GiSeok/Java/BOJ_14719.java
@@ -11,6 +11,8 @@ public class BOJ_14719 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         st.nextToken();
         
+
+        // 얘는 투 포인터 방식
         int W = Integer.parseInt(st.nextToken());
         int[] map = new int[W];
 
@@ -19,16 +21,17 @@ public class BOJ_14719 {
             map[i] = Integer.parseInt(st.nextToken());
         
         int ans = 0;
-        for (int i = 0; i < W; i++) {
-            for (int j = i + 2; j < W; j++) {
-                int min = Math.min(map[i], map[j]);
-                for (int z = i + 1; z < j; z++) {
-                    if (map[z] < min) {
-                        ans += (min - map[z]);
-                        map[z] += (min - map[z]);
-                    }
-                }
-            }
+        for (int idx = 1; idx < W-1; idx++) {
+            int left = 0, right = 0;
+
+            for (int i = 0; i < idx; i++)
+                left = Math.max(left, map[i]);
+            
+            for (int i = idx + 1; i < W; i++)
+                right = Math.max(right, map[i]);
+            
+            if (map[idx] < left && map[idx] < right)
+                ans += (Math.min(left, right) - map[idx]);
         }
 
         System.out.println(ans);
