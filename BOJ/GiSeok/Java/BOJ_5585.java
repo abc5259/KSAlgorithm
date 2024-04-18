098f8fdb0f0a1dd81985b394446201e2c8ed2ffc
GiSeok Kang <ime.rokz@gmail.com>
2024-04-18 20:50:15 +0900

    5585 - 거스름돈

--- BOJ/GiSeok/Java/BOJ_5585.java
+++ BOJ/GiSeok/Java/BOJ_5585.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_14916 {
+public class BOJ_5585 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
