722071d63b7852fa55b713ef3f3166e33402206d
GiSeok Kang <ime.rokz@gmail.com>
2024-04-20 19:43:43 +0900

    1700 - 멀티탭 스케줄링 refactor

--- BOJ/GiSeok/Java/BOJ_1700.java
+++ BOJ/GiSeok/Java/BOJ_1700.java
@@ -9,44 +9,44 @@ public class BOJ_1700 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        boolean[] use = new boolean[101];
+
         String[] nk = br.readLine().split(" ");
         int N = Integer.parseInt(nk[0]);
         int K = Integer.parseInt(nk[1]);
 
         int[] schedule = new int[K];
-        String[] used = br.readLine().split(" ");
-        for (int i = 0; i < K; i++)
-            schedule[i] = Integer.parseInt(used[i]);
+        String[] things = br.readLine().split(" ");
+
+        int cnt = 0;
+        for (int i = 0; i < K; i++) {
+            schedule[i] = Integer.parseInt(things[i]);
+            if (cnt < N && !use[schedule[i]]) { use[schedule[i]] = true; cnt++; }
+        }
         
         int ans = 0;
-        int cnt = 0;
-        boolean[] use = new boolean[101];
-
-        for (int i = 0; i < K; i++) {
+        for (int i = N; i < K; i++) {
             int idx = schedule[i];
 
             if (!use[idx]) {
-                if (cnt < N) { use[idx] = true; cnt++; }
-                else {
-                    ArrayList<Integer> list = new ArrayList<>();
-                    for (int j = i + 1; j < K; j++) {
-                        if (use[schedule[j]] && !list.contains(schedule[j]))
-                            list.add(schedule[j]);
-                    }
-
-                    if (list.size() != N) {
-                        for (int j = 0; j < i; j++) {
-                            if (use[schedule[j]] && !list.contains(schedule[j])) {
-                                use[schedule[j]] = false;
-                                break;
-                            }
-                        }
-                    } else
-                        use[list.get(list.size() - 1)] = false;
-
-                    use[idx] = true;
-                    ans++;
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int j = i + 1; j < K; j++) {
+                    if (use[schedule[j]] && !list.contains(schedule[j]))
+                        list.add(schedule[j]);
                 }
+
+                if (list.size() != N) {
+                    for (int j = 0; j < i; j++) {
+                        if (use[schedule[j]] && !list.contains(schedule[j])) {
+                            use[schedule[j]] = false;
+                            break;
+                        }
+                    }
+                } else
+                    use[list.get(list.size() - 1)] = false;
+
+                use[idx] = true;
+                ans++;
             }
         }
 
