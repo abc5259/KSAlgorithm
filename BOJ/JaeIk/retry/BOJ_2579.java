b01bce53c81fb81ad99058a7fe8a23da6ca74349
Choijake <shameless8@naver.com>
2024-04-26 15:46:40 +0900

    dp-계단오르기

---
+++ BOJ/JaeIk/retry/BOJ_2579.java
@@ -0,0 +1,31 @@
+package BOJ.JaeIk.retry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] stairs = new int[n+1];
+
+        for(int i=1; i<=n; i++){
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n+1];
+        dp[1] = stairs[1];
+
+        if(n>=2){
+            dp[2] = stairs[1] + stairs[2];
+        }
+
+        for(int i=3; i<=n; i++){
+            dp[i] = Math.max(dp[i-2], dp[i-3]+stairs[i-1]) + stairs[i];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
