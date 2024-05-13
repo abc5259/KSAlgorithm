52b0e5bf4ff5c4a1572529923819ad1104e7509d
Choijake <shameless8@naver.com>
2024-05-13 15:07:29 +0900

    자가복제문자열

--- BOJ/JaeIk/practice/swea/자가복제문자열.java
+++ BOJ/JaeIk/practice/swea/자가복제문자열.java
@@ -5,29 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 자가복제문자열 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        for(int tc=0; tc<T; tc++){
-            int k = Integer.parseInt(br.readLine())-1;
+        for(int tc=0; tc<T; tc++) {
+            Long k = Long.parseLong(br.readLine())-1;
 
-            int result=0;
-            while(k>=0){
-                if(k%2==1){
-                    k=(k-1)/2;
+            int answer = 0;
+            while(k>=0) {
+                if(k%2==1) {
+                    k = (k-1)/2;
                 }
-                if(k%4==0){
-                    result=0;
+                else if(k%2==0 && k%4==0) {
+                    answer = 0;
                     break;
                 }
-                else if(k%2==0){
-                    result=1;
+                else if(k%2==0) {
+                    answer = 1;
                     break;
                 }
             }
 
-            System.out.println("#"+(tc+1)+" "+result);
+            System.out.println("#"+(tc+1)+" "+answer);
         }
     }
 }
