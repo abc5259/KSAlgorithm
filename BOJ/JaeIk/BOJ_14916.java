5afa1c8b4376566f503198e8178e01afad34f9e5
Choijake <shameless8@naver.com>
2024-05-20 19:39:29 +0900

    거스름돈

---
+++ BOJ/JaeIk/BOJ_14916.java
@@ -0,0 +1,31 @@
+package BOJ.JaeIk;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_14916 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while(true){
+            if(n%5 == 0){
+                count += n/5;
+                break;
+            }
+            else {
+                n -= 2;
+                count++;
+            }
+
+            if(n < 0){
+                count = -1;
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
