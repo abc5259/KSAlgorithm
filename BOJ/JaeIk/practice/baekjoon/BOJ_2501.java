44f36a1a35395a95ea871fdb7119656da5075bd2
Choijake <shameless8@naver.com>
2024-05-22 16:40:54 +0900

    약수구하기

---
+++ BOJ/JaeIk/practice/baekjoon/BOJ_2501.java
@@ -0,0 +1,29 @@
+package BOJ.JaeIk.practice.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_2501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(n%i == 0)list.add(i);
+        }
+
+
+        int answer = (list.size()>=k)?list.get(k-1):0;
+
+        System.out.println(answer);
+    }
+}
