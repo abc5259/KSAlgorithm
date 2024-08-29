e311bc2898d7617be7f753b77c857e69823ee0e2
GiSeok Kang <ime.rokz@gmail.com>
2024-08-29 14:25:39 +0900

    10844 - 쉬운 계단 수 [성공(반례힌트)|01:14:18]

--- BOJ/GiSeok/Java/retry/BOJ_10844.java
+++ BOJ/GiSeok/Java/retry/BOJ_10844.java
@@ -1,36 +1,32 @@
+/**
+ * 10844 - 쉬운 계단 수 [성공(반례힌트)|01:14:18]
+ * 실버1, DP, 시도5
+ */
 package BOJ.GiSeok.Java.retry;
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class BOJ_10844 {
+    // 시간제한 1초, 메모리제한 256MB
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n][10];
+        for (int i = 0; i < 10; i++) dp[0][i] = 1;
 
-        long[][] dp = new long[N + 1][10];
-
-        // i번째 자리 뒤에 1~9 가 올 수 있는 경우의 수
-        dp[1][0] = 0;
-        for (int i = 1; i < 10; i++)
-            dp[1][i] = 1;
-
-        for (int i = 2; i <= N; i++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < 10; j++) {
-                if (j == 0)
-                    dp[i][j] = dp[i-1][j+1];
-                else if (j == 9)
-                    dp[i][j] = dp[i-1][j-1];
-                else
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000;
+                if (j == 0) dp[i][j] = dp[i-1][j+1];
+                else if (j == 9) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000;
             }
         }
 
-        long ans = 0;
-        for (int i = 0; i < 10; i++)
-            ans = (ans + dp[N][i]) % 1000000000;
-        
-        System.out.println(ans);
+        int ret = 0;
+        for (int i = 1; i < 10; i++) ret = (ret + dp[n - 1][i]) % 1000000000;
+
+        System.out.println(ret);
     }
 }
