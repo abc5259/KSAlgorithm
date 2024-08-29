0ad73c379a87613b307a46fd10a8a7832bd52d9b
GiSeok Kang <ime.rokz@gmail.com>
2024-08-29 13:43:41 +0900

    1463 - 1로 만들기 [성공|00:37:07]

--- BOJ/GiSeok/Java/retry/BOJ_1463.java
+++ BOJ/GiSeok/Java/retry/BOJ_1463.java
@@ -1,28 +1,30 @@
+/**
+ * 1463 - 1로 만들기 [성공|00:37:07]
+ * 실버3, DP, 시도3
+ */
 package BOJ.GiSeok.Java.retry;
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class BOJ_1463 {
+    // 시간제한 0.15초, 메모리제한 128MB
+    static int[] dp;
+    static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        dp = new int[1000001];
 
-        int[] dp = new int[N + 1];
-        dp[0] = 0;
         dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = 1000001;
-            if (i % 2 == 0)
-                dp[i] = Math.min(dp[i/2] + 1, dp[i]);
-            if (i % 3 == 0)
-                dp[i] = Math.min(dp[i/3] + 1, dp[i]);
-            
-            dp[i] = Math.min(dp[i - 1] + 1, dp[i]);
+            dp[i] = Math.min(dp[i], dp[i-1] + 1);
         }
 
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
