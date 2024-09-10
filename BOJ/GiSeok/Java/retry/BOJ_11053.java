6db3bdade1a2937a300289809b419d393af5f6d5
GiSeok Kang <ime.rokz@gmail.com>
2024-09-10 23:59:01 +0900

    11053 - 가장 긴 증가하는 부분 수열 [성공|00:02:00]

--- BOJ/GiSeok/Java/retry/BOJ_11053.java
+++ BOJ/GiSeok/Java/retry/BOJ_11053.java
@@ -1,9 +1,8 @@
 /**
- * 11053 - 가장 긴 증가하는 부분 수열 [성공|00:19:47]
- * 실버2, DP, 시도1
+ * 11053 - 가장 긴 증가하는 부분 수열 [성공|00:02:00]
+ * 실버2, LCS/DP, 시도1
  */
 package BOJ.GiSeok.Java.retry;
-
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -14,24 +13,22 @@ public class BOJ_11053 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) A[i] = Integer.parseInt(st.nextToken());
 
         int[] dp = new int[n];
         dp[0] = 1;
-
+        int ret = dp[0];
         for (int i = 1; i < n; i++) {
-            int max = 1;
-            for (int j = n-1; j >= 0; j--) {
-                if (arr[j] < arr[i]) max = Math.max(max, dp[j] + 1);
+            int len = 0;
+            for (int j = i-1; j >= 0; j--) {
+                if (A[i] > A[j]) len = Math.max(len, dp[j]);
             }
 
-            dp[i] = max;
+            dp[i] = len + 1;
+            ret = Math.max(ret, dp[i]);
         }
-
-        int ret = 0;
-        for (int i = 0; i < n; i++) ret = Math.max(ret, dp[i]);
 
         System.out.println(ret);
     }
