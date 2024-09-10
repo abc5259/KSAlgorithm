799c2ee02a8a781159bf51412328e4d1765646c9
GiSeok Kang <ime.rokz@gmail.com>
2024-09-10 23:03:57 +0900

    14003 - 가장 긴 증가하는 부분 수열 5 [실패|02:18:54]

--- BOJ/GiSeok/Java/BOJ_14003.java
+++ BOJ/GiSeok/Java/BOJ_14003.java
@@ -1,12 +1,14 @@
 /**
- * 12015 - 가장 긴 증가하는 부분 수열 2 [실패]
- * 골드2, LIS/이분탐색, 시도6
+ * 14003 - 가장 긴 증가하는 부분 수열 5 [실패|02:18:54]
+ * 플래5, LIS/이분탐색, 시도5
  */
 package BOJ.GiSeok.Java;
 import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_12015 {
+public class BOJ_14003 {
     // 시간제한 3초, 메모리제한 512MB
 
     public static void main(String[] args) throws IOException {
@@ -18,12 +20,17 @@ public class BOJ_12015 {
         for (int i = 0; i < n; i++) A[i] = Integer.parseInt(st.nextToken());
 
         int[] lis = new int[n];
-        lis[0] = A[0];
-        int idx = 0;
+        Arrays.fill(lis, Integer.MIN_VALUE);
+        int[] pos = new int[n];
+        lis[0] = A[0]; int idx = 0;
+        pos[0] = 0;
+
         for (int i = 0; i < n; i++) {
-            if (lis[idx] < A[i]) lis[++idx] = A[i];
-            else {
-                int low = 0;
+            if (lis[idx] < A[i]) {
+                lis[++idx] = A[i];
+                pos[i] = idx;
+            } else {
+                int low = -1;
                 int high = idx;
 
                 while (low + 1 < high) {
@@ -34,12 +41,24 @@ public class BOJ_12015 {
                 }
 
                 lis[high] = A[i];
+                pos[i] = high;
             }
         }
 
         int ret = 0;
-        for (int i = 0; i < n+1; i++) if (lis[i] != 0) ret++;
+        for (int i = 0; i < n; i++) if (lis[i] != Integer.MIN_VALUE) ret++;
 
         System.out.println(ret);
+
+        ArrayDeque<Integer> stk = new ArrayDeque<>();
+        for (int i = n-1; i >= 0; i--) {
+            if (pos[i] == ret-1) {
+                stk.push(A[i]);
+                ret--;
+            }
+        }
+
+        while (!stk.isEmpty()) System.out.print(stk.pop() + " ");
+        System.out.println();
     }
 }
