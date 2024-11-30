ee0c20d7fb3991362d8072b375a4444e5acc485e
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-30 22:55:40 +0900

    A+B-2

---
+++ BOJ/JaeHoon/BOJ_2558.java
@@ -0,0 +1,15 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        System.out.println(Integer.parseInt(a)+Integer.parseInt(b));
+    }
+}
