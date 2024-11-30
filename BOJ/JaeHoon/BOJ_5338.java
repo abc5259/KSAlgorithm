e31459ffb6e27435e99280dbd5a5ed46174fc870
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-30 23:10:39 +0900

    마이크로소프트 로고

---
+++ BOJ/JaeHoon/BOJ_5338.java
@@ -0,0 +1,11 @@
+package BOJ.JaeHoon;
+
+public class BOJ_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n"
+                + "'-..-'|   ||   |\n"
+                + "'-..-'|_.-;;-._|\n"
+                + "'-..-'|   ||   |\n"
+                + "'-..-'|_.-''-._|");
+    }
+}
