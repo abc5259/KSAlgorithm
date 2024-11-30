4bcc70992e474707be3f8c287a671dc9c271f9df
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-30 23:00:15 +0900

    크냐?

---
+++ BOJ/JaeHoon/BOJ_4101.java
@@ -0,0 +1,24 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_4101 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) break;
+
+            sb.append(a > b ? "Yes\n" : "No\n");
+        }
+
+        System.out.print(sb);
+    }
+}
