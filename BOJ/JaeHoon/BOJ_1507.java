3702fc7f70042e54b4c013ef3afc6b8c7cf87697
LeeJaeHoon <dlwogns3413@naver.com>
2025-01-10 13:46:18 +0900

    [G2 플로이드워셜] 궁금한 민호 - O, 01:10

--- BOJ/JaeHoon/BOJ_1507.java
+++ BOJ/JaeHoon/BOJ_1507.java
@@ -3,76 +3,103 @@ package BOJ.JaeHoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_1507 {
-    static class Node {
-        int v;
-        int w;
+    static int N;
+    static int[][] answer;
+    static boolean[][] used;
+    static int[][] dist;
+    public static final int MAX = 2150 * 60;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        answer = new int[N+1][N+1];
+        dist = new int[N+1][N+1];
+        used = new boolean[N+1][N+1];
+        for(int i=1; i<=N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=N; j++) {
+                answer[i][j] = Integer.parseInt(st.nextToken());
+                if(i != j) {
+                    dist[i][j] = MAX;
+                }
+            }
+        }
 
-        public Node(int v, int w) {
-            this.v = v;
+        int sum = 0;
+        for(int j=1; j<=N; j++) {
+            int min = Integer.MAX_VALUE;
+            for(int i=1; i<=N; i++) {
+                if (j == i) continue;
+                min = Math.min(min, answer[i][j]);
+            }
+
+            for(int i=1; i<=N; i++) {
+                if(answer[i][j] == min && dist[i][j] == MAX) {
+                    dist[i][j] = min;
+                    dist[j][i] = min;
+                    sum+=min;
+                }
+            }
+        }
+
+        solve();
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a,b) -> a.w - b.w);
+        for(int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                if(answer[i][j] < dist[i][j] && !used[i][j]) {
+                    used[i][j] = true;
+                    used[j][i] = true;
+                    pq.offer(new Edge(i, j, answer[i][j]));
+                }
+                else if(answer[i][j] > dist[i][j]) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+
+        if(pq.isEmpty()) {
+            System.out.println(sum);
+            return;
+        }
+
+        while (!pq.isEmpty()) {
+            Edge poll = pq.poll();
+            if(dist[poll.v1][poll.v2] == poll.w) continue;
+            if(dist[poll.v1][poll.v2] < poll.w) {
+                System.out.println(-1);
+                return;
+            }
+            dist[poll.v1][poll.v2] = poll.w;
+            dist[poll.v2][poll.v1] = poll.w;
+            sum += poll.w;
+            solve();
+        }
+
+        System.out.println(sum);
+    }
+
+    static class Edge {
+        int v1,v2,w;
+
+        public Edge(int v1, int v2, int w) {
+            this.v1 = v1;
+            this.v2 = v2;
             this.w = w;
         }
     }
-    static int[] cost;
-    static int[] indegree;
-    static List<List<Node>> graph;
-    static Queue<Node> q = new LinkedList<>();
-    static int[] time;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-
-        cost = new int[N+1];
-        time = new int[N+1];
-        indegree = new int[N+1];
-        Arrays.fill(cost, 0);
-        graph = new ArrayList<>();
-        for(int i=0; i<=N; i++) graph.add(new ArrayList<>());
-
-
-        for(int i=1; i<=N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int t = Integer.parseInt(st.nextToken());
-            indegree[i] = Integer.parseInt(st.nextToken());
-            if(indegree[i] == 0) {
-                q.offer(new Node(i,t));
-                cost[i] = t;
-            }
-            for(int j=1; j<=indegree[i]; j++) {
-                int v1 = Integer.parseInt(st.nextToken());
-                graph.get(v1).add(new Node(i, t));
+    public static void solve() {
+        for(int k=1; k<=N; k++) {
+            for(int i=1; i<=N; i++) {
+                for (int j=1; j<=N; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
             }
         }
-
-        System.out.println(bfs());
-    }
-
-    public static int bfs() {
-
-        int max = 0;
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            max = Math.max(max, cur.w);
-            for(Node next: graph.get(cur.v)) {
-                if(cost[next.v] < cur.w + next.w) {
-                    cost[next.v] = cur.w + next.w;
-                }
-                indegree[next.v]--;
-                if(indegree[next.v] == 0) {
-                    q.offer(new Node(next.v, cost[next.v]));
-                }
-            };
-        }
-
-        return max;
     }
 }
