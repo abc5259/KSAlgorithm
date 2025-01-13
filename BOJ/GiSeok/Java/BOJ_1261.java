bf12f5928287399f56439086ed9767b333dbf291
GiSeok Kang <ime.rokz@gmail.com>
2025-01-13 21:13:22 +0900

    [G4 다익스트라] 알고스팟 - O, 00:49:51 (필요없는 부분 수정..)

--- BOJ/GiSeok/Java/BOJ_1261.java
+++ BOJ/GiSeok/Java/BOJ_1261.java
@@ -19,8 +19,6 @@ public class BOJ_1261 {
     private static boolean[][] visited;
     private static int n, m;
 
-    private static int ret = Integer.MAX_VALUE;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -41,36 +39,6 @@ public class BOJ_1261 {
 
         dijkstra(0, 0);
         System.out.println(shortestPath[n-1][m-1]);
-    }
-
-    public static void dfs(int y, int x, int wall) {
-
-        System.out.println("y: " + y + ", x: " + x + ", wall: " + wall);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(maze[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        if (y == n-1 && x == m-1) {
-            ret = Math.min(ret, wall);
-            return;
-        }
-
-        visited[y][x] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            if (visited[ny][nx]) continue;
-
-            if (maze[ny][nx] == 1) dfs(ny, nx, wall + 1);
-            else dfs(ny, nx, wall);
-        }
     }
 
     public static void dijkstra(int y, int x) {
