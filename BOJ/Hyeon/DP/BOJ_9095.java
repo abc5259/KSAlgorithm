0060fecf99708eafa07e62b58d98065c9be6ce6c
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-01-25 22:27:18 +0900

    [S3 DP]-9095

---
+++ BOJ/Hyeon/DP/BOJ_9095.java
@@ -0,0 +1,29 @@
+package BOJ.Hyeon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int [] dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < dp.length; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        while (T-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
+// 접근을 했더니 n의 수에 따라 규칙적인 것을 발견
+// 이전수가 다음 수에 영향을 끼치는 것을 점화식을 이용하여
+// DP 계산
