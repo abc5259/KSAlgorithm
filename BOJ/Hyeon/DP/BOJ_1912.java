4fe192a2233d9c7d0cbfee0854f30cd71b57e2a0
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-01-28 23:23:41 +0900

    [S2 DP]-1912

---
+++ BOJ/Hyeon/DP/BOJ_1912.java
@@ -0,0 +1,32 @@
+package BOJ.Hyeon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < dp.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = dp[0] = arr[0];
+
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(max);
+    }
+}
+
+// 메모이네이션을 통해 앞의 값을 저장하기 위헤 두 배열로 만들어서 입력배열과 최종 최대값을 저장하는
+// dp를 두어서 최대값 연산을 통해 계산한다.
