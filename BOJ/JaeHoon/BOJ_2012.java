05b814428da1421e7256e932a87cb6c3a99971b3
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-01 23:42:33 +0900

    [S3 그리디] 등수 매기기 - O, 00:07

---
+++ BOJ/JaeHoon/BOJ_2012.java
@@ -0,0 +1,25 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_2012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        for(int i=1; i<=N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        long sum = 0;
+        for(int i=1; i<=N; i++) {
+            sum += Math.abs(arr[i] - i);
+        }
+
+        System.out.println(sum);
+    }
+}
