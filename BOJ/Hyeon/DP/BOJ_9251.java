dee318602ad68c7e59128a076efcec6b13dd1c19
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-04 23:48:48 +0900

    [G5 DP] LCS - X

---
+++ BOJ/Hyeon/DP/BOJ_9251.java
@@ -0,0 +1,26 @@
+package BOJ.Hyeon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] c1 = br.readLine().toCharArray();
+        char[] c2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[c1.length + 1][c2.length + 1];
+
+        for (int i = 1; i <= c1.length; i++) {
+            for (int j = 1; j <= c2.length; j++) {
+                if (c1[i - 1] == c2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[c1.length][c2.length]);
+    }
+}
