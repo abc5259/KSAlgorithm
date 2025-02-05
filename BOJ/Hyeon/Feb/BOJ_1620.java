8930eec86f9b591ec056dbeace3982209b1544d9
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-05 21:29:16 +0900

    [S4 HashMap] 포켓몬 마스터 이다솜 - Another Sol

--- BOJ/Hyeon/Feb/BOJ_1620.java
+++ BOJ/Hyeon/Feb/BOJ_1620.java
@@ -14,22 +14,17 @@ public class BOJ_1620 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> strKey = new HashMap<>(N);
-        HashMap<Integer, String> intKey = new HashMap<>(N);
+        HashMap<String, String> map = new HashMap<>();
 
         for (int i = 1; i <= N; i++) {
             String name = br.readLine();
-            strKey.put(name, i);
-            intKey.put(i, name);
+            String idx = String.valueOf(i);
+            map.put(name, idx);
+            map.put(idx, name);
         }
 
         while (M-- > 0) {
-            String str = br.readLine();
-            if (strKey.containsKey(str)) {
-                sb.append(strKey.get(str)).append("\n");
-            } else {
-                sb.append(intKey.get(Integer.parseInt(str))).append("\n");
-            }
+            sb.append(map.get(br.readLine())).append("\n");
         }
         System.out.println(sb);
     }
@@ -41,3 +36,7 @@ public class BOJ_1620 {
 // 해쉬맵의 경우 키값을 인덱스로 하고 밸류를 값으로 가지는데 포켓몬의 이름을 넣었을 때는 순번인덱스가
 // 순번인덱스를 넣었을 경우 포켓몬의 이름이 값으로 출력되어야 한다
 // 이름을 키로 가지는 해쉬맵과, 순번을 키로 가지는 해쉬 맵을 통해서 풀었다.
+
+// additional solution
+// 천재의 풀이를 봤다. 굳이 해쉬 맵을 2개로 가르지 않고 인덱스 또한, 문자열로 치환해서 구한다면 조건분기도
+// 벨류값을 얻는 것도 너무 간편하다.
