1396a516742aec519762031342ed0517f031cffa
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-07 23:18:09 +0900

    [S4 스택] 스택 - O

---
+++ BOJ/Hyeon/brute/BOJ_2961.java
@@ -0,0 +1,57 @@
+package BOJ.Hyeon.brute;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ_2961 {
+    static int[][] tastes;
+    static boolean[] sel;
+
+    static int min;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        tastes = new int[N][2];// 신맛 쓴맛
+        sel = new boolean[N]; //각 원소가 선택되었는지 여부
+
+        min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            tastes[i][0] = Integer.parseInt(st.nextToken());
+            tastes[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        subSet(0, 1, 0);
+        System.out.println(min);
+    }
+
+    static void subSet(int idx, int sour, int bitter) {
+        if (N == idx) {
+            int allFalse = 0;
+            for (boolean b : sel) {
+                if (b)
+                    continue;
+                allFalse++;
+            }
+            if (allFalse == N) {
+                return;
+            }
+            min = Math.min(min, Math.abs(sour - bitter));
+            return;
+        }
+        sel[idx] = true;
+        subSet(idx + 1, tastes[idx][0] * sour, tastes[idx][1] + bitter);
+        sel[idx] = false;
+        subSet(idx + 1, sour, bitter);
+    }
+}
+// S2 도영이가 만든 맛있는 음식
+// 부분집합 문제
+// 재귀활용
+// retry
---
+++ BOJ/Hyeon/feb/BOJ_10828.java
@@ -0,0 +1,56 @@
+package BOJ.Hyeon.feb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        StringTokenizer st;
+
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    stack.push(x);
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.peek()).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
+// S4 10828 스택
+// 풀었던거 다시푸는데 저번에는 구현으로 풀고 이번에는 Stack 컬렉션 이용해서 쉽게 풀었다.
