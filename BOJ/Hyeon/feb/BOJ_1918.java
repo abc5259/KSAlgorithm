6221478683aeefb33b0944368dd14cd37ad79724
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-09 02:45:34 +0900

    [G4 스택] 문자열 폭발 - O, retry + comment

--- BOJ/Hyeon/feb/BOJ_1918.java
+++ BOJ/Hyeon/feb/BOJ_1918.java
@@ -3,8 +3,6 @@ package BOJ.Hyeon.feb;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Stack;
 
 public class BOJ_1918 {
@@ -12,11 +10,10 @@ public class BOJ_1918 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         String str = br.readLine();
-        int len = str.length();
 
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             switch (c) {
                 case '+':
@@ -32,10 +29,13 @@ public class BOJ_1918 {
                     stack.push(c);
                     break;
                 case ')':
-                    while (!stack.isEmpty() && stack.peek() != '(') {
-                        sb.append(stack.pop());
+                    while (!stack.isEmpty()) {
+                        if (stack.peek() != '(') {
+                            sb.append(stack.pop());
+                        } else {
+                            stack.pop();
+                        }
                     }
-                    stack.pop();
                     break;
                 default:
                     sb.append(c);
@@ -64,8 +64,8 @@ public class BOJ_1918 {
 // 까지가 흔하게 아는 후위표기식인데
 // 사칙연산과 괄호의 경우 우선순위가 있다. 그래서 우선순위를 리턴받는
 // priority 메소드를 통해서 우선순위를 만든다. 근데 닫힌괄호를 만나면 안에있는걸 빼서 하는데
-// 닫힌 괄호 만나기 전에는 괄호가 가장 우선순위가 낮다는 점이 가장 중요한 포인트이다
+// 닫힌 괄호 만나기 전에는 괄호가 가장 우선순위가 낮다는 점이 가장 중요한 포인트이다 ->  열린괄호가 제일 늦게 나가기 위해
 // 열린괄호 이후 -나 + * 등은 괄호의 )거보다 우선순위가 높아서 스택에 그대로 넣어야 한다. == 닫힌괄호가 나와야 팝 할 수 있기 때문
 //-----------------------
 // trouble shooting
-// 스택 특성 EmptyStackException 예외를 생각해서 항상 스택이 비어있는지를 검사해야한다.
+// 스택 특성 EmptyStackException 예외를 생각해서 스택에서 꺼낼 때 항상 스택이 비어있는지를 검사해야한다.
--- BOJ/Hyeon/feb/BOJ_9935.java
+++ BOJ/Hyeon/feb/BOJ_9935.java
@@ -21,15 +21,17 @@ public class BOJ_9935 {
             char c = str.charAt(i);
             stack.push(c);
             if (stack.size() >= len) {
-                boolean b = true;
+                boolean check = false;
                 for (int j = 0; j < len; j++) {
-                    if (stack.get(stack.size() - len + j) != bomb.charAt(j)) {
-                        b = false;
+                    if (stack.get(stack.size() - len + j) == bomb.charAt(j)) {
+                        check = true;
+                    } else {
+                        check = false;
                         break;
                     }
                 }
-                if (b) {
-                    for (int j = 0; j < len; j++) {
+                if (check) {
+                    for (int k = 0; k < len; k++) {
                         stack.pop();
                     }
                 }
@@ -45,4 +47,8 @@ public class BOJ_9935 {
         System.out.println(sb);
     }
 }
-// 일단 풀긴했고 1시간뒤에 다시 풀자.
+// G4 스택
+// 일단 전체적으로 문자들을 순서대로 스택에 넣는다. 그 스택에서 폭탄 문자열 갯수만큼을 검사해나간다. 근데 현재 스택 개수가 폭탄보다 커야
+// 폭탄의 유무를 알 수 있고 현재 스택개수 - 문자열의 개수 하면 제일 끝 문자열만 비교할 수 있는 인덱스를 구할 수 있다.
+// 예를들면 123을 스택에 넣었고 폭탄 문자열이 23이라면 12일때는 23과 달라서 폭탄을 감지못하지만 size가 3일경우
+// 3 - 2를 통해 1인덱스인 2와 2인덱스인 3을 비교하여 폭탄을 감지 할 수 있다.
