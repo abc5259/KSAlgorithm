44ddd6578ea858d58635884d912ab05a77f1dc87
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-09 03:50:42 +0900

    [S4 스택] 괄호 - O, 00:13

---
+++ BOJ/Hyeon/feb/BOJ_9012.java
@@ -0,0 +1,40 @@
+package BOJ.Hyeon.feb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            Stack<Character> stack = new Stack<>();
+            String str = br.readLine();
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c == '(') {
+                    stack.push(c);
+                } else {
+                    if (!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                        break;
+                    }
+                }
+            }
+            if (!stack.isEmpty()) {
+                sb.append("NO").append("\n");
+            } else {
+                sb.append("YES").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
+// S4 괄호 스택문제
+// 걍 쉽게 풀었다. 예전에 푼거 리마인드 느낌으로 닫힌 괄호랑 짝맞춰서 pop 해주고 스택에 남아있으면 NO 없으면 YES
