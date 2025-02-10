50ac397cf5dc050febc3c3b476270ae6b33ef31c
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-10 13:03:35 +0900

    올바른 괄호 - 스택

--- Programmers/Hyeon/lv2/올바른괄호.java
+++ Programmers/Hyeon/lv2/올바른괄호.java
@@ -3,27 +3,29 @@ package Programmers.Hyeon.lv2;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-class Solution {
-    boolean solution(String s) {
+public class 올바른괄호 {
+    class Solution {
+        boolean solution(String s) {
 
-        Deque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            switch (c) {
-                case '(':
-                    stack.push(c);
-                    break;
-                case ')':
-                    if (!stack.isEmpty() && stack.peek() == '(') {
-                        stack.pop();
-                    } else {
+            Deque<Character> stack = new ArrayDeque<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                switch (c) {
+                    case '(':
                         stack.push(c);
-                    }
-                    break;
+                        break;
+                    case ')':
+                        if (!stack.isEmpty() && stack.peek() == '(') {
+                            stack.pop();
+                        } else {
+                            stack.push(c);
+                        }
+                        break;
+                }
             }
+            return stack.isEmpty();
         }
-        return stack.isEmpty();
     }
-}
 // lv2 올바른 괄호 스택
 // 걍 쉽게 풀었다.
+}
