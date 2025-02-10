23568eea95970d7f3c287b9ea06a55e21ace0e97
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-11 00:06:42 +0900

    [G4 큐] 좋은 친구 - O, retry 시간 초과 해결

--- BOJ/Hyeon/feb/BOJ_3078.java
+++ BOJ/Hyeon/feb/BOJ_3078.java
@@ -14,23 +14,32 @@ public class BOJ_3078 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        ArrayDeque<Integer>[] queue = new ArrayDeque[21];
+        for (int i = 2; i < 21; i++) {
+            queue[i] = new ArrayDeque<>();
+        }
 
-        int res = 0;
+        long res = 0;
 
         for (int i = 0; i < N; i++) {
             int len = br.readLine().length();
-            while (!queue.isEmpty() && i - queue.peek()[0] > K) {
-                queue.poll();
+            while (!queue[len].isEmpty() && i - queue[len].peek() > K) {
+                queue[len].poll();
             }
-            for (int[] ints : queue) {
-                if (ints[1] == len) {
-                    res++;
-                }
-            }
-            queue.offer(new int[]{i, len});
+            res += queue[len].size();
+
+            queue[len].offer(i);
         }
         System.out.println(res);
     }
 }
-// 시간 초과
+// 시간 초과 발생 -> 큐에서 탐색을 할 때는 인덱스로 하지말고 직접 접근 하도록
+// G4 좋은 친구 큐
+// 처음에 큐라고 판단하고 큐에다가 인덱스와 길이를 넣어서 인덱스를 제한으로 두고 길이가 같을 때의 수를 구하려 했지만
+// 이렇게하면 이중 반복이 된다. 그래서 O(N * K) 이기 때문에 시간초과가 발생했다.
+
+// 그렇다면 같을 때의 수를 어떻게 구하냐?
+// 똑같이 while 반복문의 비었을 때와 큐에다가 인덱스를 푸시할 때 인덱스에서 큐의 가장 앞인 peek의 값을 뺏을 때 K보다 큰 경우는
+// 좋은 친구의 조건이 아니기 때문에 이때 peek를 빼면된다== peek에는 더이상 좋은친구가 생길 수 없기 때문
+// 슬라이딩 윈도우
+// 그래서 빼고 나면 이름의 길이 배열 큐에 사이즈만큼 누적해서 더해준다.
