1c7f4c125475ba348ee0f385391b76b6dc5f241a
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-11 10:40:23 +0900

    카드 뭉치 - 큐

--- Programmers/Hyeon/lv1/카드_뭉치.java
+++ Programmers/Hyeon/lv1/카드_뭉치.java
@@ -1,6 +1,7 @@
 package Programmers.Hyeon.lv1;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class 카드_뭉치 {
     public static void main(String[] args) {
@@ -14,10 +15,8 @@ public class 카드_뭉치 {
 
     static class Solution {
         public String solution(String[] cards1, String[] cards2, String[] goal) {
-            ArrayDeque<String> q1 = new ArrayDeque<>();
-            for (String s : cards1) {
-                q1.offer(s);
-            }
+            ArrayDeque<String> q1 = new ArrayDeque<>(Arrays.asList(cards1));
+
             ArrayDeque<String> q2 = new ArrayDeque<>();
             for (String s : cards2) {
                 q2.offer(s);
