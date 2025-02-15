0380aeade36e0c2659b80456b08d16fa9252ac25
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-15 15:03:48 +0900

    [S2 BFS] 너비 우선 탐색 2 - O, 00:12

--- BOJ/Hyeon/dfsbfs/BOJ_24445.java
+++ BOJ/Hyeon/dfsbfs/BOJ_24445.java
@@ -5,22 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BOJ_24444 {
+public class BOJ_24445 {
     static ArrayList<Integer>[] adjList;
     static int[] visit;
-
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
         adjList = new ArrayList[N + 1];
-
         for (int i = 1; i <= N; i++) {
             adjList[i] = new ArrayList<>();
         }
@@ -35,10 +32,11 @@ public class BOJ_24444 {
         }
 
         for (int i = 1; i <= N; i++) {
-            Collections.sort(adjList[i]);
+            adjList[i].sort(Collections.reverseOrder());
         }
 
         visit = new int[N + 1];
+
         bfs(R);
         for (int i = 1; i <= N; i++) {
             sb.append(visit[i]).append("\n");
@@ -65,9 +63,11 @@ public class BOJ_24444 {
     }
 }
 
-// S2 너비 우선 탐색 1 알고리즘 수업
+// S2 너비 우선 탐색 2 알고리즘 수업
 // DFS 풀었던 문제와 다른 BFS이다.
 // 이때 BFS는 단순하게 만나는 노드를 큐에 족족 넣으며 BFS 큐에 값이 존재할 경우
 // 그 값을 poll해서 빼고 그 poll 한 정점이 가지고 있는 인접리스트를 참고하여 방문여부가 없는
 // 노드를 큐에 다시 삽입하고 큐가 다빠질 때까지 이를 반복해서
 // visit 배열을 통해 순번을 정한다.
+
+// 내림차순 이기에 Collections.reverseOrder()를 사용한다.
