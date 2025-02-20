d812b91ad69161c7f4b2ceaf7901c93897bfd60b
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-20 22:13:14 +0900

    [G3 BFS] 말이 되고픈 원숭이 - O, 00:57

--- BOJ/Hyeon/dp/BOJ_1890.java
+++ BOJ/Hyeon/dp/BOJ_1890.java
@@ -9,18 +9,31 @@ public class BOJ_1890 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[][] board = new int[N][N];
         int[][] dp = new int[N][N];
-
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
-                dp[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
 
-        System.out.println(dp[N - 1][N - 1]);
+        dp[0][0] = board[0][0];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int tmp = board[i][j];
+                if (tmp + i >= 0 && tmp + i < N) {
+                    dp[tmp + i][j] = board[i][j] + board[tmp + i][j];
+                }
+                if (tmp + j >= 0 && tmp + j < N) {
+
+                }
+            }
+        }
+
+        System.out.println();
     }
 }
