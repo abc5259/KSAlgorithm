3081dd115644a68a6df6e939e56f70b183991a29
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-23 00:03:12 +0900

    [S1 DP] 구간 합 구하기 5 - O, 00:21

---
+++ BOJ/Hyeon/dp/BOJ_11660.java
@@ -0,0 +1,40 @@
+package BOJ.Hyeon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken()) + dp[i][j - 1];
+            }
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int sum = 0;
+            for (int i = y1; i <= y2; i++) {
+                sum += dp[i][x2] - dp[i][x1 - 1];
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
