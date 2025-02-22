f356e516876c2ee5a8bb79f65a0925a7d6ffe47b
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-22 18:23:19 +0900

    [G3 DFS] 게리맨더링 - 추가 Solution

--- BOJ/Hyeon/retry/BOJ_17471_1.java
+++ BOJ/Hyeon/retry/BOJ_17471_1.java
@@ -1,95 +1,68 @@
 package BOJ.Hyeon.retry;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-// 전체 구역을 2개의 선거구 < = 부분집합 <= select[] 에 선택, 비선택을 true, false 로 기록
-// <= 두 개의 선거구는 선택된 구역, 비선택된 구역 나눈 개념
-// 그래프 이므로 나뉘어진 선거구 내의 구역이 모두 연결 여부
-//    <= 각 선거구의 임의의 한 구역부터 출발 모든 구역에 갈 수 있는 지 완탐 (bfs, dfs)
-//    <= 완탐을 통해 visit 배열 1개에 갈 수 있는 곳을 모두 기록
-//    <= 완탐 이후 visit 배열 확인
-// 모두 연결되어 있다면 인구수의 차를 계산, min 처리
-// dfs
 public class BOJ_17471_1 {
-    static int N, min;
-    static int[][] matrix; // i, j 정점번호는 아니다.
-    static boolean[] select; // 부분집합 처리용도
-    static boolean[] visit; // 모든 구역이 연결되어 있는 지 확인 용도
+    static int N; // 구역의 개수  1~6까지
+    static int[] people; // 인구 분포
+    static ArrayList<Integer>[] area; // 이거는 index 별로 각 구역을 나타내고 가장 처음 숫자는 각구역의 크기, 그 이후로는 인접한 구역이다
+    static boolean[] select; // 부분집합을 선택했는지에 대한 배열 A와 B 선거구
+    static int min; // 최소값 출력
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        matrix = new int[N + 1][N + 1]; // 어차피 맨 앞이 더미 <= 그 자리에 인구수를 저장
+        people = new int[N + 1];
+        area = new ArrayList[N + 1];
         select = new boolean[N + 1];
-        visit = new boolean[N + 1];
 
-        // 인구수
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            matrix[i][0] = Integer.parseInt(st.nextToken());
+            people[i] = Integer.parseInt(st.nextToken());
+            area[i] = new ArrayList<>();
         }
 
-        // 정점별 연결
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) { // i는 구역
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken()); // 인접한 정점 수
-            for (int j = 1; j <= n; j++) {
-                int v = Integer.parseInt(st.nextToken());
-                matrix[i][j] = v;  // matrix[i][v] = true 와 비교
+            int cnt = Integer.parseInt(st.nextToken()); // 이거는 인접 구역의 개수
+
+            for (int j = 0; j < cnt; j++) {
+                area[i].add(Integer.parseInt(st.nextToken()));
             }
         }
+        // 인접한 개수만큼 반복문 돌리고 입력
 
-        // 풒이
         min = Integer.MAX_VALUE;
+        // 결과값 비교 만약 값이 안들어오면 -1출력 최소값
+        // 그다음 부분집합 ? -> 두 선거구에 포함된 == 한 선거구는 포함 다른 선거구는 미포함 == true false로 구분
+        subSet(1); //   1번 선거구 부터
 
-        subset(1);
-
-        if (min == Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(min);
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
     }
 
-    static void subset(int srcIdx) {
-        // 기저조건
-        if (srcIdx == N + 1) {
-            // 부분집합 경우의 수 1개 완성 <= select 배열에 true, false 기록
+    static void subSet(int idx) {
+        if (idx == N + 1) {
             check();
             return;
         }
+        select[idx] = true;
+        // 찬성 쪽에 고른 부분집합 == A
+        subSet(idx + 1);
 
-        select[srcIdx] = true;
-        subset(srcIdx + 1);
-        select[srcIdx] = false;
-        subset(srcIdx + 1);
+        select[idx] = false;
+        // 반대 쪽에 고른 부분집합 == B
+        subSet(idx + 1);
     }
 
-    // v 정점에서 갈 수 있는 다른 정점 방문 visit에 기록
-    static void dfs(int v, boolean sel) {
-        visit[v] = true;
+    static boolean[] visit;
 
-        for (int i = 1; i <= N; i++) {
-            int adj = matrix[v][i];
-            if (adj == 0 || visit[adj] || select[adj] != sel) continue;
-
-            dfs(adj, sel);
-        }
-    }
-
-
-    // 나뉘어진 2 개 선거구가 유효 ( 적어도 1개 이상, 다 연결 )
-    // 유효하다면 두 선거구의 인구수를 계산, 차이의 최소값 처리
     static void check() {
-        // visit 배열을 이용해서 연결여부 확인
-        Arrays.fill(visit, false);
+        visit = new boolean[N + 1];
 
-        // A 선거구 ( select 배열 true 인 구역 )
-        // 선거구에 속한 구역 1개를 선택 완탐 진행 => 갈 수 있는 곳에 visit 기록
         int a = -1;
         for (int i = 1; i <= N; i++) {
             if (select[i]) {
@@ -102,8 +75,6 @@ public class BOJ_17471_1 {
 
         dfs(a, true);
 
-        // B 선거구 ( select 배열 false 인 구역 )
-        // 선거구에 속한 구역 1개를 선택 완탐 진행 => 갈 수 있는 곳에 visit 기록
         int b = -1;
         for (int i = 1; i <= N; i++) {
             if (!select[i]) {
@@ -116,20 +87,38 @@ public class BOJ_17471_1 {
 
         dfs(b, false);
 
-        // 두 선거구 모두 연결 확인
         for (int i = 1; i <= N; i++) {
-            if (!visit[i]) return; // 어떤 선거구의 구역이 연결 X
+            if (!visit[i]) {
+                return;
+            }
         }
+        // 모든 구역 방문 확인
 
-        // A, B 선거구의 인구수를 계산, 최소값 갱신
+        // 최소값 갱신
         int sumA = 0;
         int sumB = 0;
 
         for (int i = 1; i <= N; i++) {
-            if (select[i]) sumA += matrix[i][0];
-            else sumB += matrix[i][0];
+            if (select[i]) {
+                sumA += people[i];
+            } else {
+                sumB += people[i];
+            }
         }
-
         min = Math.min(min, Math.abs(sumA - sumB));
     }
+
+    static void dfs(int v, boolean sel) {
+        visit[v] = true;
+
+        for (int node : area[v]) {
+            if (visit[node] || select[node] != sel) {
+                continue;
+            }
+            dfs(node, sel);
+        }
+    }
 }
+
+// G3 게리맨더링 DFS
+// 방식은 같다.
