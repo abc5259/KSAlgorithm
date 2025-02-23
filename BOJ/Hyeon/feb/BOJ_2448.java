138e2c855d9ce9cf2d0c1efa4052ee73762556fc
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-23 23:26:42 +0900

    [G5 백트래킹] 치킨 배달 - O, 01:20

---
+++ BOJ/Hyeon/feb/BOJ_2448.java
@@ -0,0 +1,18 @@
+package BOJ.Hyeon.feb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2448 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < i + 3; j++) {
+
+            }
+        }
+    }
+}
