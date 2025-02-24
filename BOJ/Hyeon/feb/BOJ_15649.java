f9c0326c1b85deaa5fe2773b9c6c2b8d04e1f8bb
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-24 23:56:30 +0900

    [S3 백트래킹] N과 M(1) - O, 00:33

--- BOJ/Hyeon/feb/BOJ_15649.java
+++ BOJ/Hyeon/feb/BOJ_15649.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_15650 {
+public class BOJ_15649 {
     static int N, M;
-    static boolean[] visit;
     static int[] arr;
+    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,34 +20,35 @@ public class BOJ_15650 {
         visit = new boolean[N];
         arr = new int[M];
 
-        comb(0, 0);
+        perm(0);
         System.out.println(sb);
     }
 
     static StringBuilder sb = new StringBuilder();
 
-    static void comb(int idx, int depth) {
-        if (M == depth) {
-            for (int i : arr)
-                sb.append(i).append(" ");
+    static void perm(int depth) {
+        if (depth == M) {
+            for (int p : arr) {
+                sb.append(p).append(" ");
+            }
             sb.append("\n");
             return;
         }
-        for (int i = idx; i < N; i++) {
+
+        for (int i = 0; i < N; i++) {
             if (!visit[i]) {
                 visit[i] = true;
                 arr[depth] = i + 1;
-                comb(i + 1, depth + 1);
+                perm(depth + 1);
                 visit[i] = false;
             }
         }
     }
 }
 
-// S3 N과 M(2) 백트래킹 조합
-// 걍 백트래킹 관련해서
-// 방문 여부 visit 배열과 조합 메소드 사용
-// 일단 값을 출력해야되는데 1 ~ N 까지의 숫자인데 이는 visit 배열의 인덱스로 처리가능하다.
-// 만약 0번 인덱스가 t이면 1이 출력되어야하고 N이 4면 3번 인덱스까지 있고 3번인덱스 선택시 4를 출력하면된다.
-// depth로 선택을 할 수 있고 idx로 인덱스를 관리하면된다.
-// idx가 N보다 크지않게 조건을 걸어서 comb 재귀를 하면된다.
+// S3 N과 M (1) 백트래킹
+// 일단 순열 문제다
+// 순열이랑 조합의 차이를 이해하고 백트래킹으로 분석해보자
+// 순열은 중복 숫자 상관없이
+// 계속해서 출력한다 Permutation 그래서 조합이랑 다르게 방문여부만 확인하고 이전 숫자도 가져오기 때문에
+// 반복문으로 사용해보는게 편할 것이다.
