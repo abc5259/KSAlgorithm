37c0016726989d7f894d93dcdabf4a1eeec7a25a
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-02-25 11:23:11 +0900

    [S4 정렬] 보물 - O, 00:05

---
+++ BOJ/Hyeon/feb/BOJ_1026.java
@@ -0,0 +1,39 @@
+package BOJ.Hyeon.feb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ_1026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        Integer[] B = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(B, Collections.reverseOrder());
+        int res = 0;
+
+        for (int i = 0; i < N; i++) {
+            res += A[i] * B[i];
+        }
+        System.out.println(res);
+    }
+}
+
+// S4 보물 정렬
+// 개쉬움 reverseorder 학습 : 복습
