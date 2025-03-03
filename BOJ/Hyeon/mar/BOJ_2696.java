c2fa2d29c5c8f22d83ff4aba56605c9805a52b11
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-03-03 17:13:07 +0900

    [G2 우선순위 큐] 중앙값 구하기 - O, 00:25

--- BOJ/Hyeon/mar/BOJ_2696.java
+++ BOJ/Hyeon/mar/BOJ_2696.java
@@ -8,29 +8,31 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_2696 {
-    static StringTokenizer st;
-    static int cnt;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
+
         while (T-- > 0) {
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
             int M = Integer.parseInt(br.readLine());
             sb.append((M + 1) / 2).append("\n");
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+            int cnt = 0;
 
             while (M > 0) {
                 if (M > 10) {
+                    M -= 10;
                     cnt = 10;
-                    M = M - 10;
                 } else {
                     cnt = M;
                     M = 0;
                 }
+
                 st = new StringTokenizer(br.readLine());
                 for (int i = 0; i < cnt; i++) {
                     int num = Integer.parseInt(st.nextToken());
@@ -39,7 +41,6 @@ public class BOJ_2696 {
                     } else {
                         minHeap.offer(num);
                     }
-
                     if (maxHeap.size() > minHeap.size() + 1) {
                         minHeap.offer(maxHeap.poll());
                     } else if (minHeap.size() > maxHeap.size()) {
@@ -56,3 +57,10 @@ public class BOJ_2696 {
         System.out.println(sb);
     }
 }
+
+// G2 중앙값 구하기 우선순위 큐
+// 최대힙, 최소힙을 통해서 중간값을 구한다.
+// 이때 출력문이 까다로웠는데 그냥 문제가 이상한거고
+// M값이 10개가 넘어가면 입력을 이어서 못받아서 이를 위한 반복문을 한개 더 만들었다.
+// M이 0보다 클때까지 반복하는 루프에다가 사용했다.
+// 만약 최소힙의 크기가 최대힙보다 커지면 값을 이동하고, 최대힙의 크기가 최소힙보다 2개더많은경우 최소힙으로 이동한다.
