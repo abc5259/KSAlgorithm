12fd5c8de2067062ba4718bcf3c708e18d0a7815
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-16 14:27:46 +0900

    [G5 DP] 크리보드 - O, 00:29

---
+++ BOJ/JaeHoon/BOJ_11058.java
@@ -0,0 +1,25 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11058 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i=4; i<101; i++) {
+            dp[i] = dp[i-1] + 1;
+            int cnt = 2;
+            for(int j=i-3; j>=1;j--) {
+                dp[i] = Math.max(dp[i], dp[j] * cnt++);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
