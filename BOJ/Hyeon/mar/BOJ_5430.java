86d1ff934a7dcdd28399388c51cfce389a3fa71a
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-03-28 14:45:05 +0900

    [G5 Deque] AC - O, 00:43, retry

--- BOJ/Hyeon/mar/BOJ_5430.java
+++ BOJ/Hyeon/mar/BOJ_5430.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class BOJ_5430 {
     public static void main(String[] args) throws IOException {
@@ -13,50 +14,49 @@ public class BOJ_5430 {
         StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             char[] p = br.readLine().toCharArray();
-
             int n = Integer.parseInt(br.readLine());
-
-            String str = br.readLine();
-            String[] split = str.substring(1, str.length() - 1).split(",");
-
+            
             ArrayDeque<Integer> deque = new ArrayDeque<>();
 
-            for (int i = 0; i < n; i++) {
-                deque.offer(Integer.parseInt(split[i]));
+            StringTokenizer st = new StringTokenizer(br.readLine(), "[],");
+            while (n-- > 0) {
+                deque.offer(Integer.parseInt(st.nextToken()));
             }
 
             boolean flag = true;
             boolean err = false;
-            for (int i = 0; i < p.length; i++) {
-                if (!deque.isEmpty()) {
-                    if (p[i] == 'R') {
-                        flag = !flag;
-                    } else {
-                        if (flag) {
-                            deque.pollFirst();
-                        } else {
-                            deque.pollLast();
-                        }
-                    }
+            for (char command : p) {
+                if (command == 'R') {
+                    flag = !flag;
                 } else {
-                    err = true;
-                    break;
+                    if (deque.isEmpty()) {
+                        err = true;
+                        break;
+                    }
+                    if (flag) {
+                        deque.pollFirst();
+                    } else {
+                        deque.pollLast();
+                    }
                 }
             }
+
             if (err) {
                 sb.append("error").append("\n");
             } else {
                 sb.append("[");
-                while (deque.size() != 1) {
-                    if (flag) {
-                        sb.append(deque.pollFirst()).append(",");
-                    } else {
-                        sb.append(deque.pollLast()).append(",");
+                while (!deque.isEmpty()) {
+                    sb.append(flag ? deque.pollFirst() : deque.pollLast());
+                    if (!deque.isEmpty()) {
+                        sb.append(",");
                     }
                 }
-                sb.append(deque.poll()).append("]").append("\n");
+                sb.append("]").append("\n");
             }
         }
         System.out.println(sb);
     }
 }
+
+// G5 AC DEQUE
+// 메모리 초과
