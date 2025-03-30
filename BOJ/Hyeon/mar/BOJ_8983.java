750a3c0bf6a32632914a92ba4d5aeb1f2072d2b2
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-03-30 21:13:54 +0900

    [G4 이분탐색] 사냥꾼 - O, 02:02

--- BOJ/Hyeon/mar/BOJ_8983.java
+++ BOJ/Hyeon/mar/BOJ_8983.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_8983 {
-    static int L;
+    static int L; // binary 에서 쓰기
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +21,7 @@ public class BOJ_8983 {
         for (int i = 0; i < M; i++) {
             saro[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(saro);
+        Arrays.sort(saro); // 사로를 이분탐색
 
         int cnt = 0;
 
@@ -29,15 +29,15 @@ public class BOJ_8983 {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            if (binarySearch(saro, x, y)) {
+
+            if (lowerBound(saro, x, y)) {
                 cnt++;
             }
         }
-
         System.out.println(cnt);
     }
 
-    private static boolean binarySearch(int[] arr, int x, int y) {
+    private static boolean lowerBound(int[] arr, int x, int y) {
         int lo = -1;
         int hi = arr.length;
 
@@ -55,3 +55,11 @@ public class BOJ_8983 {
         return false;
     }
 }
+
+// G4 사냥꾼 이분탐색
+// 사로를 이분 탐색해서 동물을 대입하고 사로를 움직여서 정확한 조건을 통과할경우 true를 반환하고 아니면 flase를 반환 근데
+// 동물을 반복해서 대입하기 때문에 동물을 2차원 배열로 만들지 않고 그냥 x와 y 값으로 대입해서 lowerbound 한다.
+// lowerbound는 사로보다 작거나 같은 x를 구하기 때문이다.
+// 이렇게 이분탐색으로 접근해야 N * log M 이 가능하다 N*M 일 경우 10만 ^2 이기에 시간초과 발생한다.
+// 그리고 이분탐색해서 매번 인덱스나 값을 반환했지만 boolean 타입으로 리턴하고 탈출 조건에 맞을 경우 탐색할 수 있게 적용 가능했다.
+// lower bound의 의미인 lo = -1, hi = length 이다.
