201229498fbfbe760a38b2f436f01a0e2de5895d
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-04-07 16:20:54 +0900

    [S3 정렬] 빈도 정렬 - O, sol1 00:53

--- BOJ/Hyeon/retry/BOJ_2910.java
+++ BOJ/Hyeon/retry/BOJ_2910.java
@@ -3,45 +3,63 @@ package BOJ.Hyeon.retry;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_2910 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        int[][] times = new int[C + 1][3];
-        st = new StringTokenizer(br.readLine());
-        int prior = 0;
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (times[num][0] == 0) {
-                times[num][1] = prior++;
-                times[num][2] = num;
-            }
-            times[num][0]++;
-        }
-        Arrays.sort(times, (o1, o2) -> {
-            if (o2[0] == o1[0]) {
-                return o1[1] - o2[1];
-            }
-            return o2[0] - o1[0];
-        });
+        Map<Integer, Sequence> map = new HashMap<>();
+        List<Sequence> list = new ArrayList<>();
 
-        for (int i = 0; i <= C; i++) {
-            if (times[i][0] != 0) {
-                for (int j = 0; j < times[i][0]; j++) {
-                    System.out.print(times[i][2] + " ");
-                }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            if (!map.containsKey(value)) {
+                Sequence s = new Sequence(value, 0, i);
+                map.put(value, s);
+                list.add(s);
             }
+            map.get(value).cnt++;
+        }
+        Collections.sort(list);
+        for (Sequence s : list) {
+            while (s.cnt-- > 0) {
+                sb.append(s.num).append(" ");
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+    static class Sequence implements Comparable<Sequence> {
+        int num;
+        int cnt;
+        int prior;
+
+        public Sequence(int num, int cnt, int prior) {
+            this.num = num;
+            this.cnt = cnt;
+            this.prior = prior;
+        }
+
+        @Override
+        public int compareTo(Sequence o) {
+            if (o.cnt == this.cnt) {
+                return this.prior - o.prior;
+            }
+            return o.cnt - this.cnt;
         }
     }
 }
 
 // S3 빈도 정렬 해시
 // 메모리 초과났음 왜냐하면 C가 10억까지 되니까 인트 인덱스가 수도 없이 커짐 그래서 숫자를 인덱스로 관리하면안된다.
+// 4바이트 * 3 * 10억 == 120억바이트라서 크기가 128메가 바이트보다 현저히 크다.
+// 다른 풀이 if 조건을 걸었고 map과 정렬된 list를 사용
