9eb97c5af781b6bd302a2a5a4c9409f47e2fe7a5
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-04-21 23:57:15 +0900

    [S3 브루트포스] 숫자 야구 - O, 00:51

---
+++ BOJ/Hyeon/BOJ_2503.java
@@ -0,0 +1,71 @@
+package BOJ.Hyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2503 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] visit = new boolean[988];
+
+        for (int i = 123; i < 988; i++) {
+            String s = String.valueOf(i);
+            if (s.charAt(0) == '0' || s.charAt(1) == '0' || s.charAt(2) == '0') {
+                continue;
+            }
+            if (s.charAt(0) == s.charAt(1) || s.charAt(1) == s.charAt(2) || s.charAt(2) == s.charAt(0)) {
+                continue;
+            }
+            visit[i] = true;
+        }
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for (int i = 123; i < 988; i++) {
+                if (visit[i]) {
+                    int sCnt = 0;
+                    int bCnt = 0;
+
+                    char[] req = String.valueOf(num).toCharArray();
+                    char[] ans = String.valueOf(i).toCharArray();
+
+                    for (int j = 0; j < 3; j++) {
+                        for (int k = 0; k < 3; k++) {
+                            if (req[j] == ans[k]) {
+                                if (j == k) {
+                                    sCnt++;
+                                } else {
+                                    bCnt++;
+                                }
+                            }
+                        }
+                    }
+
+                    if (sCnt != s || bCnt != b) {
+                        visit[i] = false;
+                    }
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 123; i < 988; i++) {
+            if (visit[i]) {
+                res++;
+            }
+        }
+        System.out.print(res);
+    }
+}
+
+// S3 숫자 야구 브루트포스
+// 일단 풀기는 했는데 좀 더 나은 방향으로 리팩토링 중이다
+// 브루트포스 문제 특성상 쉽게 접근하지 못하는 단점을 고쳐야만 한다.
