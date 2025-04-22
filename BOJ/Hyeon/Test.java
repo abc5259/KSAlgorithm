732b2f564b7af11d629f02ce71677604803cb4e7
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-04-22 22:18:41 +0900

    [G5 브루트포스] 나3곱2 - O, 01:22 comment

--- BOJ/Hyeon/Test.java
+++ BOJ/Hyeon/Test.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_16936 {
+public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -19,6 +19,7 @@ public class BOJ_16936 {
         Arrays.sort(B);
 
         long[] tmp;
+
         for (int i = 0; i < N; i++) {
             tmp = new long[N];
             tmp[0] = B[i];
@@ -27,10 +28,9 @@ public class BOJ_16936 {
             while (idx < N - 1) {
                 long a = tmp[idx] * 2;
                 boolean isValid = false;
-
                 for (int j = 0; j < N; j++) {
                     if (B[j] == a) {
-                        tmp[++idx] = a;
+                        tmp[++idx] = B[j];
                         isValid = true;
                         break;
                     }
@@ -63,11 +63,3 @@ public class BOJ_16936 {
         }
     }
 }
-
-// G5 나3곱2 브루트포스
-// 브루트포스 답게 일단 나3곱2에 대해서 주어진 뒤죽박죽수열을 오름차순으로 정렬하고 1개씩 비교해가면서 한다.
-// 모든 수열의 숫자에 있어서 곱2가 되는지 안된다면 나3이 되는지 안된다면 바로 다음 수열로 진행한다.
-// 이는 가장 먼저 시작하는 수열을 찾기 위해서 이고 가장 먼저 시작하는 수열을 찾을 경우 idx를 통해 while문으로 계쏙해서 반복한다.
-// 중간에 곱2로 된다면 검사할 필요없이 다음 곱2나 나3을 찾기 위해 다시 반복문으로 진행해야되기 때문에 break 를 걸고 continue 문으로 탈출한다.
-// 그런데 2개의 조건 곱2 , 나3 둘다 안맞을 경우 해당 while문을 종료하고 다음 수열의 숫자를 전달 받는다.
-// 힘들게 스스로 풀었다.
