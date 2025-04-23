403fd921742a28cef5c7572d85e2fa10fe34576f
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-04-23 23:50:38 +0900

    [S3 브루트포스] 두 스티커 - X, 01:35

---
+++ BOJ/Hyeon/BOJ_16937.java
@@ -0,0 +1,92 @@
+package BOJ.Hyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_16937 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] stickers = new int[N][2];
+
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            if ((R <= h && C <= w) || (R <= w && C <= h)) {
+                cnt++;
+                stickers[i][0] = R;
+                stickers[i][1] = C;
+            }
+        }
+
+        if (cnt < 2) {
+            System.out.println(0);
+            return;
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (stickers[i][0] <= h && stickers[i][1] <= w) {
+                    if (stickers[j][0] <= h && stickers[j][1] <= w) {
+                        // 가로로 이어 붙이기
+                        if (stickers[i][1] + stickers[j][1] <= w) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                        // 세로로 이어 붙이기
+                        if (stickers[i][0] + stickers[j][0] <= h) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                    }
+                    if (stickers[j][1] <= h && stickers[j][0] <= w) {
+                        // 가로로 이어 붙이기
+                        if (stickers[i][1] + stickers[j][0] <= w) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                        // 세로로 이어 붙이기
+                        if (stickers[i][0] + stickers[j][1] <= h) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                    }
+                }
+                if (stickers[i][1] <= h && stickers[i][0] <= w) {
+                    if (stickers[j][0] <= h && stickers[j][1] <= w) {
+                        // 가로로 이어 붙이기
+                        if (stickers[i][0] + stickers[j][1] <= w) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                        // 세로로 이어 붙이기
+                        if (stickers[i][1] + stickers[j][0] <= h) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                    }
+                    if (stickers[j][1] <= h && stickers[j][0] <= w) {
+                        // 가로로 이어 붙이기
+                        if (stickers[i][0] + stickers[j][0] <= w) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                        // 세로로 이어 붙이기
+                        if (stickers[i][1] + stickers[j][1] <= h) {
+                            res = Math.max(res, sum(stickers, i, j));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.print(res);
+    }
+
+    private static int sum(int[][] stickers, int i, int j) {
+        return (stickers[i][0] * stickers[i][1]) + (stickers[j][0] * stickers[j][1]);
+    }
+}
