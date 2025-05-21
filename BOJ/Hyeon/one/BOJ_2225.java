210c38071ef678b989970afdbdc1d4d7f6e495ad
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-21 23:50:31 +0900

    [G5 DP] 합분해 - O,retry

---
+++ BOJ/Hyeon/one/BOJ_2225.java
@@ -0,0 +1,36 @@
+package BOJ.Hyeon.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2225 {
+    static final int MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+        for (int i = 0; i <= K; i++) {
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; j <= K; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+            }
+        }
+        System.out.println(dp[N][K]);
+    }
+}
+
+// G5 합분해 DP
