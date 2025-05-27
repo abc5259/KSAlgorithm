408474313540f5ffa33144058e2511bb5e775e02
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-27 22:15:12 +0900

    [S3 DP] 퇴사 - O, 00:24, 접근 고민 시간 길었다

--- BOJ/Hyeon/retry/BOJ_14501.java
+++ BOJ/Hyeon/retry/BOJ_14501.java
@@ -1,4 +1,4 @@
-package BOJ.Hyeon.one;
+package BOJ.Hyeon.retry;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,10 +24,17 @@ public class BOJ_14501 {
             if (i + table[i][0] <= N) {
                 dp[i + table[i][0]] = Math.max(dp[i] + table[i][1], dp[i + table[i][0]]);
             }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            dp[i + 1] = Math.max(dp[i], dp[i + 1]);
         }
-        System.out.println(dp[N]);
+        System.out.print(dp[N]);
     }
 }
+
 // S3 퇴사 DP
-// 가장 간단한 메모이제이션
+// 아 어렵게 풀었다
+// 점화식 접근이 어려웠고 인덱스 관리가 힘들었다.
+
+// 일단 기존의 테이블을 자료로 사용하고 dp 로 가능한 받을 수 있는 금액을 만들어서 메모이제이션으로 비교해가면서 쓴다
+// 만약 상담이 퇴사전에 되는것이라면 dp에 종료 날 dp 에 값을 넣는데 이때 기존에 가지고 있던 최대값이랑
+// 현재 내가 회의를 끝내서 얻을 수 있는 금액과 지금까지의 dp 의 합산의 max 비교를 통해 dp에 저장하고
+// 다음 dp를 미룬다. 최대값으로
