b62596786e9e2e91d12c7d07075ce6584529e496
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-02 23:43:43 +0900

    [G5 DP] 전깃줄 - retry, 01:42, LIS

--- BOJ/Hyeon/one/BOJ_2225.java
+++ BOJ/Hyeon/one/BOJ_2225.java
@@ -11,26 +11,10 @@ public class BOJ_2225 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N + 1][K + 1];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= N; i++) {
-            dp[i][0] = 0;
-            dp[i][1] = 1;
-        }
-        for (int i = 0; i <= K; i++) {
-            dp[1][i] = i;
-        }
 
-        for (int i = 2; i <= N; i++) {
-            for (int j = 2; j <= K; j++) {
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
-            }
-        }
-        System.out.println(dp[N][K]);
     }
 }
-
-// G5 합분해 DP
---
+++ BOJ/Hyeon/one/BOJ_2565.java
@@ -0,0 +1,50 @@
+package BOJ.Hyeon.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ_2565 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] wire = new int[n][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            wire[i][0] = Integer.parseInt(st.nextToken());
+            wire[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(wire, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int[] dp = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (wire[i][1] > wire[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        System.out.print(n - max);
+    }
+}
+
+// G5 전깃줄 Dp
+// 일단 LIS 풀이인데
