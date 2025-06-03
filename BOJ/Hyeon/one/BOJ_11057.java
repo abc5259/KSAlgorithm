6aeca188ef8cef8a4111c6e7a39250334c1cd0d4
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-03 23:37:00 +0900

    [S1 DP] 오르막 수 - O, 00:51

---
+++ BOJ/Hyeon/one/BOJ_11057.java
@@ -0,0 +1,39 @@
+package BOJ.Hyeon.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11057 {
+    static final int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            int sum = numbers[0];
+            for (int j = 1; j < 10; j++) {
+                numbers[j] = (numbers[j - 1] % MOD + numbers[j] % MOD) % MOD;
+                sum = (sum + numbers[j]) % MOD;
+            }
+            dp[i] = sum;
+        }
+        System.out.println(dp[N]);
+    }
+}
+
+// S1 오르막 수 DP
+// 일단 점화식을 통해서 풀었다. 한자리의 수를 다 더하면 10이고 2자리의 수는 55 3자리는 220 이었다
+// 이는 누적합과 관련이 있었다. 현재의 값 + 이전의 값을 통해서 numbers 배열을 구하고 이 numbers 배열의 모든 수의 합을
+// dp 라는 곳에 저장해서 n에 맞는 수를 출력했다
+
+// n번의 반복을 통하고 무조건 10개의 수를 계산해야돼서 N*10의 시간복잡도와 배열의 누적합을 통해서 다음 배열에 대입하고
+// 이에 대한 누적합을 구한다.
+
+// 근데 이러면 dp가 필요없어?
