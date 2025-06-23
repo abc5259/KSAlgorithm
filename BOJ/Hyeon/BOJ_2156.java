547e67becddc60d5338ced5324e9affd33d0dfd6
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-24 03:39:46 +0900

    [S1 DP] 포도주 시식 - O, 00:54

--- BOJ/Hyeon/BOJ_2156.java
+++ BOJ/Hyeon/BOJ_2156.java
@@ -10,31 +10,26 @@ public class BOJ_2156 {
         int n = Integer.parseInt(br.readLine());
 
         int[] podo = new int[n + 1];
-
         for (int i = 1; i <= n; i++) {
             podo[i] = Integer.parseInt(br.readLine());
         }
 
         int[] dp = new int[n + 1];
+        dp[1] = podo[1];
 
-        if (n == 1) {
-            dp[1] = podo[1];
-        } else if (n == 2) {
+        if (n > 1) {
             dp[2] = podo[2] + podo[1];
-        } else {
-            dp[1] = podo[1];
-            dp[2] = podo[2] + podo[1];
-
-            for (int i = 3; i <= n; i++) {
-                dp[i] = Math.max(dp[i - 2] + podo[i], Math.max(podo[i] + dp[i - 3] + podo[i - 1], dp[i - 1]));
-            }
         }
-        System.out.println(dp[n]);
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], podo[i] + Math.max(dp[i - 3] + podo[i - 1], dp[i - 2]));
+        }
+        System.out.print(dp[n]);
     }
 }
-// 한참이나 틀렸다.
-// 조건이 3가지였는데 2가지만 비교했다. 2579와 유사한 문제였는데,
-// 3가지의 값을 줄 때 dp[i-2] + podo[i] = 는 현재값 + 한칸 뛰고 그 앞에 값들
-// dp[i-1] = 현재값 이전까지의 최고 연산
-// 마지막으로는 dp[i-3] + podo[i-1] + podo[i] 이다. 4개의 수의 경우 6 10 13 9 일 때
-// 6 + 13 + 9 거나 6 + 10 + 9 거나 10 + 13으로 3가지를 비교해야만 한다.
+
+// S1 포도주 시식 DP
+// 3가지 연속이 안된다 이거는 DP와 기존 배열을 비교해서 구분해야 된다
+// 이번에 안먹는다면 저번까지의 최대 vs 이번에 먹으면 이전꺼 먹고 먹은건지 아닌지 비교해야된다.
+// 이번꺼 먹고 이전꺼 먹었으면 그전꺼먹으면 안돼서 그전의 DP를 먹고
+// 이번꺼 먹고 이전꺼 안먹었으면 그전까지의 DP를 계산해서 비교한다.
