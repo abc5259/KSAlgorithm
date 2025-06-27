885690162011ff52308198f7068a8670478cddfc
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-27 12:26:01 +0900

    [S2 DP] 가장 긴 증가하는 부분 수열 - O, 00:19

--- BOJ/Hyeon/BOJ_11053.java
+++ BOJ/Hyeon/BOJ_11053.java
@@ -9,34 +9,31 @@ public class BOJ_11053 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
+            dp[i] = 1;
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[N];
-        for (int i = 0; i < N; i++) {
-            dp[i] = 1;
+        int max = 1;
+
+        for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                if (arr[j] < arr[i] && dp[j] >= dp[i]) {
                     dp[i] = dp[j] + 1;
                 }
             }
-        }
-
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            if (max < dp[i]) {
-                max = dp[i];
-            }
+            max = Math.max(max, dp[i]);
         }
         System.out.println(max);
     }
 }
-// 주어진 수열에서 오름차순으로 구성가능한 원소들을 선택하여 최대 길이를 찾는 것
-// 제시된 수열에서 dp 메모이제이션을 구성해야한다.
-// dp의 배열은 1로 초기화하고 arr 배열에서 이전 인덱스보다 큰 수면 작은 인덱스의 갯수 만큼 횟수의 덧셈이 있고,
-// 이때 덧셈은 이전의 수의 +1 하는 값을 dp의 값으로 가진다.
-// 이에 대한 최대값을 출력하라.
+
+// S2 가장 긴 증가하는 부분 수열 DP
+// 일단 풀었다. 이전 DP와의 값비교가 있어야 되기에 2중 반복문을 사용하고
+// 최소한 길이를 제공하기위해 DP 배열의 모든 값을 1로 초기화 시킨다
+// 그리고 max 를 통해 가장 긴 부분의 dp를 구한다.
