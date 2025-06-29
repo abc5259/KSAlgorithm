b6c5eff5ffdc47099b0544ac80620ec45ec7ce1e
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-30 02:26:35 +0900

    [S2 DP] 신나는 함수 실행 - O, 00:35

--- BOJ/Hyeon/BOJ_9184.java
+++ BOJ/Hyeon/BOJ_9184.java
@@ -6,13 +6,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_9184 {
-    static int[][][] dp = new int[21][21][21];
+    static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
+        dp = new int[21][21][21];
         while (true) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -26,15 +27,15 @@ public class BOJ_9184 {
         System.out.println(sb);
     }
 
-    private static int w(int a, int b, int c) {
-        if (((0 < a && a < 21) && (0 < b && b < 21) && (0 < c && c < 21)) && dp[a][b][c] != 0) {
-            return dp[a][b][c];
-        }
+    static int w(int a, int b, int c) {
         if (a <= 0 || b <= 0 || c <= 0) {
             return 1;
         }
         if (a > 20 || b > 20 || c > 20) {
-            return dp[20][20][20] = w(20, 20, 20);
+            return w(20, 20, 20);
+        }
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
         }
         if (a < b && b < c) {
             return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
@@ -42,6 +43,8 @@ public class BOJ_9184 {
         return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
     }
 }
-// 이미 구현된 함수에서 메모이제이션을 사용하여 값을 저장한 것을 불러오도록 하는 것이다.
-// 더해서 범위 밖의 수를 제한하여 배열을 생성하고 그 수가 입력되면 IndexOutofBound 안하게 끔
-// 조건을 처음부터 제시해주고 구한 값은 dp에 리턴 받는다.
+// S2 신나는 함수 실행 DP
+// 탑다운 방식으로 해서 기저 사례를 설정한다 문제에서 주어진 0이하와 20 초과 조건을 두고
+// 20이 넘길시에 20으로 대체해서 재귀를 활용한다. 이때
+// dp라는 메모이제이션을 활용해서 이미 사전에 계산이 되었을 경우 값만 가져가다가 쓰게끔해서 3차원 배열으로 a b c 를 활용하여
+// 탑다운 방식의 재귀, 메모이제이션을 활용한다.
