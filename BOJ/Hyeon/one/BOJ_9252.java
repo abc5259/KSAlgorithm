a90c25182f120e603b4eb665caf798b33ab17c6f
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-01 03:50:25 +0900

    [G4 DP] LCS 2 - retry, 01:03

---
+++ BOJ/Hyeon/one/BOJ_9252.java
@@ -0,0 +1,31 @@
+package BOJ.Hyeon.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9252 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+    }
+}
+
+// G4 LCS 2 DP
+// 역추적 다시 풀어보기
