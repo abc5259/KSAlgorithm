d8496510a21cc30a3d0e93e21f3e98417023ab04
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-07 03:34:52 +0900

    [S3 누적합] 수열 - O, 00:19

---
+++ BOJ/Hyeon/BOJ_18112.java
@@ -0,0 +1,36 @@
+package BOJ.Hyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_18112 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= N - K; i++) {
+            int sum = 0;
+            for (int j = i; j < i + K; j++) {
+                sum += arr[j];
+            }
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
+// S3 수열 누적합
+// 걍 풀었다. 누적해서 비교하는 문제
+// 슬라이딩 윈도우 범위조심 N - K 하고나서 +1해줘야한다.
