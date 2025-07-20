f7e66037fb94b4f6254d58ed9a68f4d90c5f5127
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-21 01:31:38 +0900

    [S1 DP] 떡 먹는 호랑이 - O, 00:33

--- BOJ/Hyeon/BOJ_2502.java
+++ BOJ/Hyeon/BOJ_2502.java
@@ -1,4 +1,4 @@
-package BOJ.Hyeon;
+package BOJ.Hyeon.one;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,13 +21,11 @@ public class BOJ_2502 {
 
         while (true) {
             dp[0] = A;
-
             for (int i = A; i < K; i++) {
                 dp[1] = i;
                 for (int j = 2; j < D - 1; j++) {
                     dp[j] = dp[j - 1] + dp[j - 2];
                 }
-
                 if (dp[D - 1] == dp[D - 2] + dp[D - 3]) {
                     System.out.println(dp[0]);
                     System.out.println(dp[1]);
@@ -40,4 +38,7 @@ public class BOJ_2502 {
 }
 
 // S1 떡 먹는 호랑이 DP
-// retry
+// 일단 고려해야될 점이 그냥 최종적인 DP[D-1] 의 값만 알고 있어서
+// 바텀업 방식으로 구현해서 dp[0] + dp[1] 의 값을 브루트포스해서
+// 찾아내어야 하고 그다음에 조건문을 걸어서
+// dp[D-1] == dp[D-2] + dp[D-3] 해서 일치하는 값을 찾아서 해야한다.
