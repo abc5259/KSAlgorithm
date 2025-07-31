1b0fc3ec5632cfc7905d4b383095a79da0fd925a
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-31 22:47:02 +0900

    [S2 DP] 상자넣기 - O, 00:14

---
+++ BOJ/Hyeon/BOJ_1965.java
@@ -0,0 +1,38 @@
+package BOJ.Hyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1965 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+        System.out.print(max);
+    }
+}
+
+// S2 상자넣기 DP
+// LIS 풀이로써 쉽게 풀었다.
