c5a86ed6a24dbec7d22f6c8321e747a0501cb99e
LeeJaeHoon <dlwogns3413@naver.com>
2025-08-01 01:03:07 +0900

    [S4 그리디] Project Teams - O, 00:05

---
+++ BOJ/JaeHoon/BOJ_20044.java
@@ -0,0 +1,27 @@
+package BOJ.JaeHoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_20044 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()) * 2;
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<N/2; i++) {
+            min = Math.min(min, arr[i] + arr[N-1-i]);
+        }
+        System.out.println(min);
+    }
+}
