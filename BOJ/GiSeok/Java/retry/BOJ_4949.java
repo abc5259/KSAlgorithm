88ce4520edf917a61fd7b6b99f35989ad4483888
GiSeok Kang <ime.rokz@gmail.com>
2025-08-06 00:27:46 +0900

    [S4 스택] 균형잡힌 세상 - O, 00:05:34

---
+++ BOJ/GiSeok/Java/retry/BOJ_4949.java
@@ -0,0 +1,34 @@
+package BOJ.GiSeok.Java.retry;
+
+// 00:05:34 S4
+import java.util.*;
+import java.io.*;
+
+public class BOJ_4949 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String 입력 = br.readLine();
+            if (입력.equals(".")) break;
+
+            Deque<Character> stk = new ArrayDeque<>();
+            for (int i = 0; i < 입력.length(); i++) {
+                char 문자 = 입력.charAt(i);
+
+                if (문자 != '[' && 문자 != ']' && 문자 != '(' && 문자 != ')') continue;
+
+                if (stk.isEmpty()) stk.push(문자);
+                else {
+                    if (문자 == ']' && stk.peek() == '[') stk.pop();
+                    else if (문자 == ')' && stk.peek() == '(') stk.pop();
+                    else stk.push(문자);
+                }
+            }
+
+            if (stk.isEmpty()) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+}
