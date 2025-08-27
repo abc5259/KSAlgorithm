f70424a5019bde24afccd39ae31cd8d2d5c157e9
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-08-28 02:38:44 +0900

    [S1 DP] 1,2,3 더하기 5 - O, 01:22 retry 로 해결

--- BOJ/Hyeon/BOJ_15990.java
+++ BOJ/Hyeon/BOJ_15990.java
@@ -1,4 +1,4 @@
-package BOJ.Hyeon.one;
+package BOJ.Hyeon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -34,4 +34,11 @@ public class BOJ_15990 {
     }
 }
 // S1 1,2,3 더하기 5 DP
-// 2차원 배열로 점화식 만들어야된다.
+// 일단 2차원 배열을 통해서 점화식을 구해나갔다 정수 1,2,3 까지는 입력값으로 관리하고
+// 최대범위인 100000을 고려해서 배열을 만든다음 1,2,3일때를 관리할 수 있는 [4] 배열을 만들고
+// 표 형태로 봐서 MOD 연산을 위한 점화식을 구했다
+// refactor
+
+// 일단 T 만큼 반복해서 DP 값을 구해야 하는데
+// 이는 반복문을 그냥 다 돌려버린다음 DP값만 합 연산 모듈러 연산해서 구하는게
+// 시간적으로 훨씬 나았다.
