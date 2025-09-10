2e7d60f2979dd6310217da951fd47821f1be99e0
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-09-11 01:58:34 +0900

    [G3 DP] 행렬 곱셈 순서 - X, 01:44, retry fail

--- BOJ/Hyeon/retry/BOJ_11049.java
+++ BOJ/Hyeon/retry/BOJ_11049.java
@@ -1,22 +1,24 @@
-package BOJ.Hyeon.one;
+package BOJ.Hyeon.retry;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 무조건 다시 풀기
 public class BOJ_11049 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[][] node = new int[N + 1][2];
-        StringTokenizer st;
+        int[][] matrix = new int[N + 1][2];
 
+        StringTokenizer st;
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            node[i][0] = Integer.parseInt(st.nextToken());
-            node[i][1] = Integer.parseInt(st.nextToken());
+
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
         }
 
         int[][] dp = new int[N + 1][N + 1];
@@ -27,7 +29,7 @@ public class BOJ_11049 {
                 dp[start][end] = Integer.MAX_VALUE;
 
                 for (int k = start; k < end; k++) {
-                    int cost = dp[start][k] + dp[k + 1][end] + node[start][0] * node[k][1] * node[end][1];
+                    int cost = dp[start][k] + dp[k + 1][end] + matrix[start][0] * matrix[k][1] * matrix[end][1];
                     dp[start][end] = Math.min(dp[start][end], cost);
                 }
             }
@@ -37,4 +39,9 @@ public class BOJ_11049 {
 }
 
 // G3 행렬 곱셈 순서 DP
-// 어렵다 너무 오래 투자했음 다시 풀어봐야될듯
+// 너무 어렵다 접근자체를 잘 모르겠다.
+// 행렬곱셈 결합 법칙은 순서에 따라 연산 횟수가 달라짐
+// DP 상태 정의 i번째 행렬부터 J번쨰 행렬까지 곱하는 연산횟수
+// 기억할 정보 : 최소비용이라는 숫자만 dp 테이블에 기록 [1][N]
+
+// [i][j] 계산 위해서 k를 이요해서 2가지로 i k / k+1 j
