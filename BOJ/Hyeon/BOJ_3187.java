a0d774b62fe40c289ff1246531cfb2e8ec810871
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-10-02 01:58:06 +0900

    [S1 BFS] 양치기 꿍 - O, 00:10

--- BOJ/Hyeon/BOJ_3187.java
+++ BOJ/Hyeon/BOJ_3187.java
@@ -1,90 +1,87 @@
-package BOJ.Hyeon.one;
+package BOJ.Hyeon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_3187 {
-    static int R, C;
+    static int R, C, wolf, sheep;
     static char[][] map;
     static boolean[][] visit;
-    static int v, k;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
         map = new char[R][C];
         visit = new boolean[R][C];
+
         for (int i = 0; i < R; i++) {
-            String str = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < C; j++) {
-                map[i][j] = str.charAt(j);
+                map[i][j] = line.charAt(j);
             }
         }
 
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if (visit[i][j]) {
-                    continue;
-                }
-                if (map[i][j] != '#') {
+                if (!visit[i][j] && map[i][j] != '#') {
                     bfs(i, j);
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(k).append(" ").append(v);
-        System.out.println(sb);
+        System.out.print(sheep + " " + wolf);
     }
 
-    static int dy[] = {1, 0, -1, 0};
-    static int dx[] = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
 
     static void bfs(int y, int x) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        visit[y][x] = true;
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{y, x});
 
-        int wolf = 0, sheep = 0;
+        visit[y][x] = true;
+
+        int v = 0, k = 0;
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
+
             int cy = poll[0];
             int cx = poll[1];
+
             if (map[cy][cx] == 'v') {
-                wolf++;
+                v++;
             } else if (map[cy][cx] == 'k') {
-                sheep++;
+                k++;
             }
 
             for (int i = 0; i < 4; i++) {
                 int ny = cy + dy[i];
                 int nx = cx + dx[i];
 
-                if (ny < 0 || nx < 0 || ny >= R || nx >= C) {
+                if (ny < 0 || nx < 0 || ny >= R || nx >= C || visit[ny][nx] || map[ny][nx] == '#') {
                     continue;
                 }
-                if (visit[ny][nx]) {
-                    continue;
-                }
-                if (map[ny][nx] != '#') {
-                    visit[ny][nx] = true;
-                    queue.offer(new int[]{ny, nx});
-                }
+                queue.offer(new int[]{ny, nx});
+                visit[ny][nx] = true;
             }
         }
-        if (sheep > wolf) {
-            k += sheep;
+
+        if (k > v) {
+            sheep += k;
         } else {
-            v += wolf;
+            wolf += v;
         }
     }
 }
 // S1 양치기 꿍 BFS
+// 10분
+// 일단 풀어봤던거라서 쉽게 풀었다.
+// poll 하고 나서 v 인지 k 인지 점검을 하고 4 방향 탐색 시에는 그냥 가볍게
+// 범위 제한만 해서 continue 아니면 offer 하고 그렇게 방문 여부 확인한다.
+// bfs 탈출 시에 조건문으로 정적변수에 합한다.
