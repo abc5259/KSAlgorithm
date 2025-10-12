485f0f3594d903cd5058bd6b2bb26e3c1ea09181
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-10-12 23:53:39 +0900

    [B1 수학] 초콜릿 자르기 - O, 00:03

---
+++ BOJ/Hyeon/BOJ_2163.java
@@ -0,0 +1,22 @@
+package BOJ.Hyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2163 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println(N * M - 1);
+    }
+}
+// B1 초콜릿 자르기 수학
+// 1분
+// 걍 풀었음
