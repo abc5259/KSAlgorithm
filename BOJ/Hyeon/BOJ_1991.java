12bb8afe377f012bde7af50d854ab336b7821c46
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-11-05 01:42:59 +0900

    [B2 그리디] 거스름돈 - O, 00:04

--- BOJ/Hyeon/BOJ_1991.java
+++ BOJ/Hyeon/BOJ_1991.java
@@ -12,18 +12,20 @@ public class BOJ_1991 {
 
         int change = 1000 - price;
 
+        int[] coins = {500, 100, 50, 10, 5, 1};
+
         int cnt = 0;
 
-        int[] coin = {500, 100, 50, 10, 5, 1};
-
-        for (int i : coin) {
-            if (change >= i) {
-                cnt += change / i;
-                change %= i;
+        for (int coin : coins) {
+            if (change >= coin) {
+                cnt += change / coin;
+                change %= coin;
             }
         }
         System.out.println(cnt);
     }
 }
 // B2 거스름돈 그리디
-// 그냥 시간업서어서 바로 풀었다.
+// 4분
+// 일단 지불한 금액과 주어진 1000에서 차이인 change 를 통해서
+// 그리디로 가장 작은 cnt를 얻기위한 반복문으로 조건을 통해서 증감식으로 계산한다.
