db3ea8fa18514a5b25ecb8a0fc5fd829e54509aa
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-11-05 21:22:00 +0900

    [S4 이분탐색] 어두운 굴다리 - O, 00:40, 오랜만의 이분탐색 어렵네

--- BOJ/Hyeon/BOJ_17266.java
+++ BOJ/Hyeon/BOJ_17266.java
@@ -6,27 +6,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_17266 {
-    static int N, M;
+    static int N;
     static int[] lamp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        M = Integer.parseInt(br.readLine());
         lamp = new int[M];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             lamp[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.print(binarySearch());
+        System.out.println(lowerBound());
     }
 
-    static int binarySearch() {
-
+    static int lowerBound() {
         int lo = 0;
         int hi = N;
 
@@ -43,11 +41,11 @@ public class BOJ_17266 {
     }
 
     static boolean check(int var) {
-        int tmp = 0;
+        int tmp = 0; // 빛이 닿은 오른쪽 좌표
 
-        for (int i = 0; i < M; i++) {
-            if (lamp[i] - var <= tmp) {
-                tmp = lamp[i] + var;
+        for (int dis : lamp) {
+            if (dis - tmp <= var) {
+                tmp = dis + var;
             } else {
                 return false;
             }
@@ -55,5 +53,10 @@ public class BOJ_17266 {
         return N <= tmp;
     }
 }
-// S4 어두운 굴다리 이분 탐색
-// 일단 풀었다.
+// S4 어두운 굴다리 이분탐색
+// 40분
+// 와 오랜만에 푸니까 이해가 안됨
+// 일단 이분탐색 인가? 굴다리를 비추기 위해 가로등의 최소 높이
+// 근데 가로등의 높이를 mid 로 정하면 굴다리 전체를 비출 수 있냐 없냐 이거는 FFF랑 TTT다 그래서 결정 문제로 반환
+// 그다음에 check 에서는 mid 즉 높이가 낮아지면 비추는 범위가 좁아서 FF 이고 높이가 개 높으면 T 일 테니
+// FFFF..TTTT 일거다 그래서 최초의 T를 구해야 되니까 lowerBound 이다
