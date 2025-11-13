bf406e6f14f2643131c1e4c307ef316a82cf15ea
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-11-13 20:02:21 +0900

    [S4 이분탐색] 제곱근 - O, 00:16, 복습

--- BOJ/Hyeon/BOJ_13706.java
+++ BOJ/Hyeon/BOJ_13706.java
@@ -6,21 +6,24 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class BOJ_13706 {
+    static BigInteger N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger N = new BigInteger(br.readLine());
 
-        System.out.print(lowerBound(N));
+        N = new BigInteger(br.readLine());
+
+        System.out.println(lowerBound());
     }
 
-    static BigInteger lowerBound(BigInteger N) {
-        BigInteger lo = BigInteger.ZERO;
+    static BigInteger lowerBound() {
+        BigInteger lo = BigInteger.valueOf(0);
         BigInteger hi = N;
 
-        while (lo.add(BigInteger.ONE).compareTo(hi) < 0) {
-            BigInteger mid = hi.subtract(lo).divide(BigInteger.valueOf(2)).add(lo);
+        while (hi.compareTo(lo.add(BigInteger.valueOf(1))) > 0) {
+            BigInteger mid = (hi.subtract(lo)).divide(BigInteger.valueOf(2)).add(lo);
 
-            if (mid.multiply(mid).compareTo(N) >= 0) {
+            if (check(mid)) {
                 hi = mid;
             } else {
                 lo = mid;
@@ -28,13 +31,14 @@ public class BOJ_13706 {
         }
         return hi;
     }
+
+    static boolean check(BigInteger mid) {
+        return (mid.multiply(mid)).compareTo(N) >= 0;
+    }
 }
-// S4 제곱근 이분 탐색
-// 15분
-// 일단 주어진 문제가 완벽하게 이분탐색이 가능하게 끔 주어졌다 N이 800자 정도라니까 10의 800승인거다,,
-// 그래서 long 보다 더 큰 문자열 형태인 BigInteger를 통해서 했고
-// 이를 활용하여서 이분탐색을 진행하였다
-// 일단 제곱근을 구하는데 항상 양의 정수로 출력된다는거부터 완벽하게 떨어지는 값이 있따고 생각하였고
-// 예를들어 36을 주었을 때 6이 나와야 된다
-// 근데 그러면 범위를 0부터 36까지 해서 반으로 줄여나가면서 해도 log 연산으로 해도 시간복잡도는 해결가능했다
-// 그리고 FFFF TTTT 범위 형식이기 때문에 lowerBound를 통해서 mid * mid >= N 으로 했다
+// S4 제곱근 이분탐색 + BigInteger
+// 16분
+// BigInteger 와 BigDecimal 에 대해서 좀 헷갈렸던 것이 있다
+// 그리고 BigInteger에 대한 메소드도 알아둬야 할 거 같다 일반 적인 사칙연산으로는 불가능 하다 그리고
+// lowerBound 를 통해서 FFFF TTTT 구조 이고 우리가 사실 원하는 값보다 크거나 같다 이므로 이게
+// 가장 처음의 T 만 구하면 된다 그 T는 일치할 테고 왜냐하면 제곱근만 주어지는 문제이기에.
