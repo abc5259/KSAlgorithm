7b8887f84048daba89627f9b01f05bca88e3fc7f
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-11-19 02:07:50 +0900

    [S3 HashMap] 파일 정리 - O, 00:07, 복습

--- BOJ/Hyeon/BOJ_20291.java
+++ BOJ/Hyeon/BOJ_20291.java
@@ -14,8 +14,8 @@ public class BOJ_20291 {
         Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
-            String file = br.readLine();
-            file = file.substring(file.indexOf('.') + 1);
+            String tmp = br.readLine();
+            String file = tmp.substring(tmp.indexOf('.') + 1);
 
             map.put(file, map.getOrDefault(file, 0) + 1);
         }
@@ -26,11 +26,14 @@ public class BOJ_20291 {
 
         StringBuilder sb = new StringBuilder();
 
-        for (String s : list) {
-            sb.append(s).append(" ").append(map.get(s)).append("\n");
+        for (String name : list) {
+            sb.append(name).append(" ").append(map.get(name)).append("\n");
         }
         System.out.print(sb);
     }
 }
-// S3 파일 정리 HashMap
-// 걍했다.
+// S3 파일 정리 HashMap 복습
+// 7분
+// 그냥 HashMap으로 만들어서 문자열과 반복 회수를 센다음에
+// 오름차순 정렬이 중요했는데 바로 List 에 다가 KeySet을 옮겨두고 여기서
+// Collection 정렬을 해버려서 사용했다.
