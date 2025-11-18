06791facea29cdacea736e69f939e3a1bc30b3ea
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-11-18 20:07:56 +0900

    [S5 완전탐색] 비밀번호 발음하기 - O, 00:25, 복습 시간 복잡도를 3N 에서 N으로 줄였음 구현을 복습하면 좋을듯

--- BOJ/Hyeon/BOJ_4659.java
+++ BOJ/Hyeon/BOJ_4659.java
@@ -7,67 +7,67 @@ import java.io.InputStreamReader;
 public class BOJ_4659 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringBuilder sb = new StringBuilder();
 
         while (true) {
-            String str = br.readLine();
-            if (str.equals("end")) {
+            String pw = br.readLine();
+            if (pw.equals("end")) {
                 break;
             }
 
-            boolean isPossible = false;
-
-            for (int i = 0; i < str.length(); i++) {
-                char tmp = str.charAt(i);
-                if (isVowel(tmp)) {
-                    isPossible = true;
-                    break;
-                }
-            }
-            if (!isPossible) {
-                sb.append("<").append(str).append("> is not acceptable.").append("\n");
-                continue;
-            }
-
-            for (int i = 0; i < str.length() - 1; i++) {
-                if ((str.charAt(i) == str.charAt(i + 1) && (str.charAt(i) != 'e' && str.charAt(i) != 'o'))) {
-                    isPossible = false;
-                    break;
-                }
-            }
-            if (!isPossible) {
-                sb.append("<").append(str).append("> is not acceptable.").append("\n");
-                continue;
-            }
-
-            for (int i = 0; i < str.length() - 2; i++) {
-                boolean vowel = isVowel(str.charAt(i));
-
-                if (vowel) {
-                    if (isVowel(str.charAt(i + 1)) && isVowel(str.charAt(i + 2))) {
-                        isPossible = false;
-                        break;
-                    }
-                } else {
-                    if (!isVowel(str.charAt(i + 1)) && !isVowel(str.charAt(i + 2))) {
-                        isPossible = false;
-                        break;
-                    }
-                }
-            }
-            if (!isPossible) {
-                sb.append("<").append(str).append("> is not acceptable.").append("\n");
+            if (checkPassword(pw)) {
+                sb.append("<").append(pw).append("> is acceptable.").append("\n");
             } else {
-                sb.append("<").append(str).append("> is acceptable.").append("\n");
+                sb.append("<").append(pw).append("> is not acceptable.").append("\n");
             }
         }
         System.out.println(sb);
+    }
+
+    static boolean checkPassword(String pw) {
+
+        boolean hasVowel = false;
+        int vCnt = 0;
+        int cCnt = 0;
+
+        for (int i = 0; i < pw.length(); i++) {
+            char cur = pw.charAt(i);
+            boolean isV = isVowel(cur);
+
+            if (isV) {
+                hasVowel = true;
+                vCnt++;
+                cCnt = 0;
+            } else {
+                cCnt++;
+                vCnt = 0;
+            }
+
+            if (cCnt == 3 || vCnt == 3) {
+                return false;
+            }
+
+            if (i > 0) {
+                char prev = pw.charAt(i - 1);
+                if (cur == prev) {
+                    if (cur != 'e' && cur != 'o') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return hasVowel;
     }
 
     static boolean isVowel(char c) {
         return c == 'a' || c == 'i' || c == 'o' || c == 'u' || c == 'e';
     }
 }
-// S5 비밀번호 발음하기 완전탐색
-// 그냥 시키는 대로 했어
+// S5 비밀번호 발음하기 완전탐색 복습
+// 25분 복습치고 풀어볼만하네 신기하
+// 일단 내가 푼거는 모든 조건을 1번씩 완탐으로 탐색하는 거였는데
+// 다시 공부할때는 찾아보니 한번의 탐색으로 조건 모두를 검사할 수 있었다
+// 먼저 pw 를 입력받고 첫번째로 이게 모음인지 여부와
+// 해당 단어가 모음이거나 자음이면 연속되는 건 없는지 연속된 개수가 3개이상인지 를 검사하고
+// 또 그 반복문이 통과되면 과거의 단어와 비교해서 일치하다면 e 나 o 가 아닌지도 점검해서
+// 최종적으로 모음을 포함했냐는 플래그를 리턴하면 되었다.
