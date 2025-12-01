8c6113d4c9b58464330d3dab7caab8546b646a10
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-12-01 20:54:17 +0900

    [S3 백트래킹] N과 M(1) - O, 00:05, 복습

--- BOJ/Hyeon/BOJ_15649.java
+++ BOJ/Hyeon/BOJ_15649.java
@@ -7,8 +7,9 @@ import java.util.StringTokenizer;
 
 public class BOJ_15649 {
     static int N, M;
-    static int[] arr;
+    static int[] res;
     static boolean[] visit;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,38 +18,39 @@ public class BOJ_15649 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        res = new int[M];
         visit = new boolean[N];
-        arr = new int[M];
 
-        perm(0);
+        sb = new StringBuilder();
+
+        dfs(0);
+
         System.out.println(sb);
     }
 
-    static StringBuilder sb = new StringBuilder();
-
-    static void perm(int depth) {
-        if (depth == M) {
-            for (int p : arr) {
-                sb.append(p).append(" ");
+    static void dfs(int idx) {
+        if (idx == M) {
+            for (int var : res) {
+                sb.append(var).append(" ");
             }
             sb.append("\n");
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i + 1;
-                perm(depth + 1);
-                visit[i] = false;
+            if (visit[i]) {
+                continue;
             }
+            res[idx] = i + 1;
+            visit[i] = true;
+            dfs(idx + 1);
+            visit[i] = false;
         }
     }
 }
-
-// S3 N과 M (1) 백트래킹
-// 일단 순열 문제다
-// 순열이랑 조합의 차이를 이해하고 백트래킹으로 분석해보자
-// 순열은 중복 숫자 상관없이
-// 계속해서 출력한다 Permutation 그래서 조합이랑 다르게 방문여부만 확인하고 이전 숫자도 가져오기 때문에
-// 반복문으로 사용해보는게 편할 것이다.
+// S1 N과 M(1) 백트래킹 DFS
+// 5분
+// 순열을 구하는 것이다 중복되는 수열을 구하지말고 중복없이 고른다.
+// 그래서 방문 여부를 통해서 내가 이전에 고른 값에 대한 여부를 판단해서 재귀하고
+// 다시 기저사례를 만나 탈출한다음 백트래킹으로 그 값의 방문여부를 취소하고 다음 단계로 넘어간다.
+// 중복을 막아야 하기 떄문에 visit 를 사용했다.
