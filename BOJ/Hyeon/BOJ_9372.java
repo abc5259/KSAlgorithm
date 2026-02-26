1f6df06acc6924f67289b5f605c87e29a3ec03a0
mookkae <ehsk2315@kyungsung.ac.kr>
2026-02-27 02:46:05 +0900

    [S4 신장 트리] 상근이의 여행 - O, 00:25, 복습 아니 BFS 인줄알았는데 문제자체가 무조건 연결그래프라 그냥 노드 개수 -1 인 간선을 구하는 것이다

--- BOJ/Hyeon/BOJ_9372.java
+++ BOJ/Hyeon/BOJ_9372.java
@@ -3,14 +3,13 @@ package BOJ.Hyeon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class BOJ_9372 {
-    static List<Integer>[] adj;
-    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int T = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
@@ -19,47 +18,23 @@ public class BOJ_9372 {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            adj = new ArrayList[N + 1];
-            visit = new boolean[N + 1];
-
-            for (int i = 1; i <= N; i++) {
-                adj[i] = new ArrayList<>();
-            }
-
-            while (M-- > 0) {
+            for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
 
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
 
-                adj[a].add(b);
-                adj[b].add(a);
             }
-            sb.append(bfs()).append("\n");
+            sb.append(N - 1).append("\n");
         }
         System.out.println(sb);
     }
-
-    static int bfs() {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(1);
-        visit[1] = true;
-
-        int edges = 0;
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            for (int next : adj[cur]) {
-                if (!visit[next]) {
-                    visit[next] = true;
-                    queue.offer(next);
-                    edges++;
-                }
-            }
-        }
-        return edges;
-    }
 }
-// S4 상근이의 여행 BFS
-// 10분
-// 그냥 방문여부로 판단해서 풀었다.
+// S4 상근이의 여행 신장 트리
+// 복습 BFS 로 풀었다가..
+// 25분
+// 아니 최단거리를 구하라고 해서 bfs 하면서 최단거리를 구하려 했는데
+// 문제를 푸니까 모든 국가는 연결되어있다 이는 연결 그래프라는 뜻이고
+// 나는 adj 를 쓰면서 퍼져나갈줄 알았는데 그냥 다 연결되어있었다.
+// 방문한 나라는 카운트를 안한다했다 이는 bfs 로 방문을 안하는 줄 알았는데 이미 연결된 곳을 또 연결 안한다
+// 그래서 맨처음 국가를 제외한 -1 국가를 방문하기 위해 노드를 구한다
