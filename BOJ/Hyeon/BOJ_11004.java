6bf61045f6e1f574ba2660e6af879dae5f4f99be
mookkae <ehsk2315@kyungsung.ac.kr>
2026-02-27 02:09:52 +0900

    [S5 정렬] K번째 수 - O, 00:05, 복습 듀얼 피벗 퀵정렬 vs 타임정렬

--- BOJ/Hyeon/BOJ_11004.java
+++ BOJ/Hyeon/BOJ_11004.java
@@ -3,9 +3,7 @@ package BOJ.Hyeon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_11004 {
@@ -16,19 +14,23 @@ public class BOJ_11004 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-
-        List<Integer> arr = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            arr.add(Integer.parseInt(st.nextToken()));
+            pq.offer(Integer.parseInt(st.nextToken()));
         }
-        Collections.sort(arr);
 
-        System.out.println(arr.get(K - 1));
+        while (K-- > 1) {
+            pq.poll();
+        }
+
+        System.out.println(pq.poll());
     }
 }
-// S5 K번째 수 정렬
-// 10분
-// 원시타입과 래퍼타입의 정렬 방식에 대한 최악의 상황 차이
+// S5 K번째 수 정렬 복습
+// 5분
+// 일단 다시 풀었는데 Arrays.sort 가 시간초과가 날거 같았다 왜냐하면
+// 시간복잡도가 500만인데 최악을 고려한다면 O(N^2) 듀얼 피벗 퀵정렬을 써서
+// List 를 통해 Collections.sort 를 해버린다면 타임정렬을 한다.최악의상황이어도 N log N
+// PQ의 힙정렬 또한 마찬가지 N log N 이다. 그리고 K 만큼 반복 O(K)
