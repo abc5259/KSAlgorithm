bede7abf67e390aae84963d908d794e63f7e507f
mookkae <ehsk2315@kyungsung.ac.kr>
2026-03-03 15:26:55 +0900

    [G5 그리디, 정렬] 회의실 배정 - O, 00:11, 복습

--- BOJ/Hyeon/BOJ_1931.java
+++ BOJ/Hyeon/BOJ_1931.java
@@ -10,14 +10,16 @@ import java.util.StringTokenizer;
 public class BOJ_1931 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
 
         int[][] meeting = new int[N][2];
 
+        StringTokenizer st;
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
+
             meeting[i][0] = Integer.parseInt(st.nextToken());
             meeting[i][1] = Integer.parseInt(st.nextToken());
         }
@@ -33,20 +35,22 @@ public class BOJ_1931 {
         });
 
         int cnt = 0;
-        int preTime = 0;
+
+        int endTime = 0;
 
         for (int i = 0; i < N; i++) {
-            if (preTime <= meeting[i][0]) {
-                preTime = meeting[i][1];
+            if (endTime <= meeting[i][0]) {
+                endTime = meeting[i][1];
                 cnt++;
             }
         }
+
         System.out.println(cnt);
     }
 }
-// G5 회의실 배정 그리디 알고리즘
-// 가장 기본 문제로써
-// 회의실 시간을 겹치지 않게 많은 스케줄을 짜는 것이 목적이다.
-// 많은 스케줄을 짜기 위해선 회의가 짧아야되고 짧은 회의는 빨리 끝나는 회의이다.
-// 그래서 빨리 끝나는 회의를 기준으로 정렬해서 comparator를 사용하고
-// 시작시간이 이전에 끝난 시간보다 클 경우 끝나는 시간을 이전으로 넣어주고 cnt++ 하면된다.
+
+// G5 회의실 배정 그리디, 정렬 복습
+// 11분
+// 그리디로써 회의가 끝나마자마 바로 시작하는게 중요하기때문에
+// 빨리 끝내는 순으로 정렬을 해버리고 endTime 보다 시작시간이 크거나 같은게 먼저 나오면 그 회의를 시작해버리면돼서
+// 해당 조건에 맞게 회의가 끝나는 시간을 오름차로 정렬하고 cnt 개수를 반환해주는 문제.
