b99e288179b4e492f2e6ab7ec5a9971f0a0b0f68
GiSeok Kang <ime.rokz@gmail.com>
2026-03-18 15:48:43 +0900

    [B4 구현/수학] 영수증 - O, 00:02:22

---
+++ BOJ/GiSeok/Java/BOJ_25304.java
@@ -0,0 +1,27 @@
+package BOJ.GiSeok.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_25304 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ans += (a * b);
+        }
+
+        if (ans == x) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
