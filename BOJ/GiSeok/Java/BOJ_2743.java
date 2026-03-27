640b8f07028b223940b06dbe92c19721fb6bbdf0
GiSeok Kang <ime.rokz@gmail.com>
2026-03-27 09:15:17 +0900

    [B5 문자열] 단어 길이 재기 - O, 00:01:11

---
+++ BOJ/GiSeok/Java/BOJ_2743.java
@@ -0,0 +1,13 @@
+package BOJ.GiSeok.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
